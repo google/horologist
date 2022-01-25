2bb787e32c2d63cfea64f52741b9447c5438520d
Yuri Schimke <yschimke@google.com>
2022-01-25 14:53:36 +0000

    fillMaxRectangle modifier.
    Similar to BoxInsetLayout, but for Wear Compose.
    Finds the correct sized rectangle for a round display to ensure no content is cropped.

---
+++ compose-layout/README.md
@@ -0,0 +1,21 @@
+# Dummy library
+
+[![Maven Central](https://img.shields.io/maven-central/v/com.google.android.horologist/horologist-dummy)](https://search.maven.org/search?q=g:com.google.android.horologist)
+
+For more information, visit the documentation: https://google.github.io/horologist/dummy
+
+## Download
+
+```groovy
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation "com.google.android.horologist:horologist-dummy:<version>"
+}
+```
+
+Snapshots of the development version are available in [Sonatype's `snapshots` repository][snap]. These are updated on every commit.
+
+  [snap]: https://oss.sonatype.org/content/repositories/snapshots/com/google/horologist/horologist-dummy/
---
+++ compose-layout/src/main/java/com/google/android/horologist/compose/layout/fillMaxRectangle.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.compose.layout
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kotlin.math.sqrt
+@Stable
+fun Modifier.fillMaxRectangle() = composed(
+  inspectorInfo = debugInspectorInfo {
+    name = "fillMaxRectangle"
+  }
+) {
+  val isRound = LocalContext.current.resources.configuration.isScreenRound
+  var inset: Dp = 0.dp
+  if (isRound) {
+    val screenHeightDp = LocalContext.current.resources.configuration.screenHeightDp
+    val screenWidthDp = LocalContext.current.resources.configuration.smallestScreenWidthDp
+    val maxSquareEdge = (sqrt(((screenHeightDp * screenWidthDp) / 2).toDouble()))
+    inset = Dp(((screenHeightDp - maxSquareEdge) / 2).toFloat())
+  }
+  padding(all = inset)
+}

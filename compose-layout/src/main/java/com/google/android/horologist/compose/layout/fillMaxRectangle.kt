82e86ff40551b29bcd9360d5e883c53b0dade204
Yuri Schimke <yschimke@google.com>
2022-01-26 09:48:14 +0000

    Review comments and tests

--- compose-layout/src/main/java/com/google/android/horologist/compose/layout/fillMaxRectangle.kt
+++ compose-layout/src/main/java/com/google/android/horologist/compose/layout/fillMaxRectangle.kt
@@ -16,11 +16,12 @@
 
 package com.google.android.horologist.compose.layout
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -41,13 +42,13 @@ public fun Modifier.fillMaxRectangle(): Modifier = composed(
     name = "fillMaxRectangle"
   }
 ) {
-  val isRound = LocalContext.current.resources.configuration.isScreenRound
+  val isRound = LocalConfiguration.current.isScreenRound
   var inset: Dp = 0.dp
   if (isRound) {
-    val screenHeightDp = LocalContext.current.resources.configuration.screenHeightDp
-    val screenWidthDp = LocalContext.current.resources.configuration.smallestScreenWidthDp
+    val screenHeightDp = LocalConfiguration.current.screenHeightDp
+    val screenWidthDp = LocalConfiguration.current.smallestScreenWidthDp
     val maxSquareEdge = (sqrt(((screenHeightDp * screenWidthDp) / 2).toDouble()))
     inset = Dp(((screenHeightDp - maxSquareEdge) / 2).toFloat())
   }
-  padding(all = inset)
+  fillMaxSize().padding(all = inset)
 }

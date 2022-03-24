2a19a6db9e345b819424bd767536c246a044ba9b
Yuri Schimke <yuri@schimke.ee>
2022-03-24 19:08:43 +0000

    Test Activity Lifecycle

--- sample/src/main/java/com/google/android/horologist/navsample/NavActivity.kt
+++ sample/src/main/java/com/google/android/horologist/navsample/NavActivity.kt
@@ -19,13 +19,19 @@ package com.google.android.horologist.navsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavHostController
+import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
 
 class NavActivity : ComponentActivity() {
+    lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            NavWearApp()
+            navController = rememberSwipeDismissableNavController()
+
+            NavWearApp(navController)
         }
     }
 }

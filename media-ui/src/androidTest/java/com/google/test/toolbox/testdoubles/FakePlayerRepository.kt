2a496632570a8e0721d5a766613c685d76fc35db
Gustavo Pagani <gustavopagani@google.com>
2022-05-05 14:12:09 +0100

    Improve stateful PlayScreen

---
+++ media-ui/src/androidTest/java/com/google/test/toolbox/testdoubles/FakePlayerRepository.kt
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(ExperimentalHorologistMediaDataApi::class)
+
+package com.google.test.toolbox.testdoubles
+
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.Player.Command
+import com.google.android.horologist.media.data.ExperimentalHorologistMediaDataApi
+import com.google.android.horologist.media.data.model.TrackPosition
+import com.google.android.horologist.media.data.repository.PlayerRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class FakePlayerRepository : PlayerRepository {
+
+    private var _availableCommandsList = MutableStateFlow(Player.Commands.EMPTY)
+    override val availableCommands: StateFlow<Player.Commands> = _availableCommandsList
+
+    private var _playing = MutableStateFlow(false)
+    override val isPlaying: StateFlow<Boolean> = _playing
+
+    private var _currentMediaItem: MutableStateFlow<MediaItem?> = MutableStateFlow(null)
+    override val currentMediaItem: StateFlow<MediaItem?> = _currentMediaItem
+
+    private var _trackPosition: MutableStateFlow<TrackPosition?> = MutableStateFlow(null)
+    override val trackPosition: StateFlow<TrackPosition?> = _trackPosition
+
+    private var _shuffleModeEnabled = MutableStateFlow(false)
+    override val shuffleModeEnabled: StateFlow<Boolean> = _shuffleModeEnabled
+
+    private var mediaItems: List<MediaItem>? = null
+    private var currentItemIndex = -1
+
+    override fun prepareAndPlay(mediaItem: MediaItem, play: Boolean) {
+        _currentMediaItem.value = mediaItem
+        if (play) {
+            _playing.value = true
+        }
+    }
+
+    override fun prepareAndPlay(mediaItems: List<MediaItem>?, startIndex: Int, play: Boolean) {
+        mediaItems?.let {
+            this.mediaItems = it
+            currentItemIndex = startIndex
+            _currentMediaItem.value = it[startIndex]
+        }
+
+        if (play) {
+            _playing.value = true
+        }
+    }
+
+    override fun seekToPreviousMediaItem() {
+        currentItemIndex--
+        _currentMediaItem.value = mediaItems!![currentItemIndex]
+    }
+
+    override fun seekToNextMediaItem() {
+        currentItemIndex++
+        _currentMediaItem.value = mediaItems!![currentItemIndex]
+    }
+
+    override fun getSeekBackIncrement(): Long? {
+        TODO("Not yet implemented")
+    }
+
+    override fun seekBack() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSeekForwardIncrement(): Long? {
+        TODO("Not yet implemented")
+    }
+
+    override fun seekForward() {
+        TODO("Not yet implemented")
+    }
+
+    override fun pause() {
+        _playing.value = false
+    }
+
+    override fun toggleShuffle() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePosition() {
+        _trackPosition.value = _trackPosition.value?.let {
+            it.copy(current = it.current + 1)
+        } ?: TrackPosition(1, 10)
+    }
+
+    fun addCommand(@Command command: Int) {
+        _availableCommandsList.value = Player.Commands.Builder()
+            .addAll(_availableCommandsList.value)
+            .add(command)
+            .build()
+    }
+}
--- media-ui/src/main/java/com/google/android/horologist/media/ui/screens/PlayScreen.kt
+++ media-ui/src/main/java/com/google/android/horologist/media/ui/screens/PlayScreen.kt
@@ -28,29 +28,94 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.google.android.horologist.media.ui.ExperimentalHorologistMediaUiApi
+import com.google.android.horologist.media.ui.components.MediaControlButtons
+import com.google.android.horologist.media.ui.components.TextMediaDisplay
 import com.google.android.horologist.media.ui.state.PlayerViewModel
+import com.google.android.horologist.media.ui.utils.StateUtils.rememberStateWithLifecycle
 
 @ExperimentalHorologistMediaUiApi
 @Composable
 public fun PlayScreen(
     playerViewModel: PlayerViewModel,
-    mediaDisplay: @Composable ColumnScope.(PlayerViewModel) -> Unit,
-    controlButtons: @Composable RowScope.(PlayerViewModel) -> Unit,
-    buttons: @Composable RowScope.(PlayerViewModel) -> Unit,
     modifier: Modifier = Modifier,
-    background: @Composable BoxScope.(PlayerViewModel) -> Unit = {}
+    mediaDisplay: (@Composable ColumnScope.() -> Unit)? = null,
+    controlButtons: (@Composable RowScope.() -> Unit)? = null,
+    showProgress: Boolean = true,
+    buttons: (@Composable RowScope.() -> Unit)? = null,
+    background: @Composable BoxScope.() -> Unit = {}
 ) {
+    val playerUiState by rememberStateWithLifecycle(flow = playerViewModel.playerUiState)
+
     PlayScreen(
-        mediaDisplay = { mediaDisplay(playerViewModel) },
-        controlButtons = { controlButtons(playerViewModel) },
-        buttons = { buttons(playerViewModel) },
+        mediaDisplay = mediaDisplay ?: {
+            TextMediaDisplay(
+                title = playerUiState.mediaItem?.title,
+                artist = playerUiState.mediaItem?.artist
+            )
+        },
+        controlButtons = controlButtons ?: {
+            PlayScreenDefaultControlButtons(
+                showProgress = showProgress,
+                onPlayClick = { playerViewModel.prepareAndPlay() },
+                onPauseClick = { playerViewModel.pause() },
+                playPauseEnabled = playerUiState.playPauseEnabled,
+                playing = playerUiState.playing,
+                percent = playerUiState.trackPosition?.percent ?: 0f,
+                onSeekToPreviousButtonClick = { playerViewModel.seekToPreviousMediaItem() },
+                seekToPreviousButtonEnabled = playerUiState.seekToPreviousEnabled,
+                onSeekToNextButtonClick = { playerViewModel.seekToNextMediaItem() },
+                seekToNextButtonEnabled = playerUiState.seekToNextEnabled,
+            )
+        },
+        buttons = buttons ?: {},
         modifier = modifier,
-        background = { background(playerViewModel) },
+        background = { background() },
     )
+}
+
+@OptIn(ExperimentalHorologistMediaUiApi::class)
+@Composable
+private fun PlayScreenDefaultControlButtons(
+    showProgress: Boolean,
+    onPlayClick: () -> Unit,
+    onPauseClick: () -> Unit,
+    playPauseEnabled: Boolean,
+    playing: Boolean,
+    percent: Float,
+    onSeekToPreviousButtonClick: () -> Unit,
+    seekToPreviousButtonEnabled: Boolean,
+    onSeekToNextButtonClick: () -> Unit,
+    seekToNextButtonEnabled: Boolean,
+) {
+    if (showProgress) {
+        MediaControlButtons(
+            onPlayClick = onPlayClick,
+            onPauseClick = onPauseClick,
+            playPauseEnabled = playPauseEnabled,
+            playing = playing,
+            percent = percent,
+            onSeekToPreviousButtonClick = onSeekToPreviousButtonClick,
+            seekToPreviousButtonEnabled = seekToPreviousButtonEnabled,
+            onSeekToNextButtonClick = onSeekToNextButtonClick,
+            seekToNextButtonEnabled = seekToNextButtonEnabled,
+        )
+    } else {
+        MediaControlButtons(
+            onPlayClick = onPlayClick,
+            onPauseClick = onPauseClick,
+            playPauseEnabled = playPauseEnabled,
+            playing = playing,
+            onSeekToPreviousButtonClick = onSeekToPreviousButtonClick,
+            seekToPreviousButtonEnabled = seekToPreviousButtonEnabled,
+            onSeekToNextButtonClick = onSeekToNextButtonClick,
+            seekToNextButtonEnabled = seekToNextButtonEnabled,
+        )
+    }
 }
 
 @ExperimentalHorologistMediaUiApi

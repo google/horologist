cb95627c9cc27a22c131924b3580915e4b3cdcd2
Gustavo Pagani <gustavopagani@google.com>
2022-05-12 23:32:23 +0100

    Add PodcastControlButtons

---
+++ media-ui/src/debug/java/com/google/android/horologist/media/ui/components/PodcastControlButtonsPreview.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(ExperimentalHorologistMediaUiApi::class)
+
+package com.google.android.horologist.media.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.horologist.media.ui.ExperimentalHorologistMediaUiApi
+import com.google.android.horologist.media.ui.components.controls.SeekButtonIncrement
+
+@Preview("Enabled - Playing - With progress")
+@Composable
+fun PodcastControlButtonsPreview() {
+    PodcastControlButtons(
+        onPlayButtonClick = {},
+        onPauseButtonClick = {},
+        playPauseButtonEnabled = true,
+        playing = true,
+        percent = 0.25f,
+        onSeekBackButtonClick = {},
+        seekBackButtonIncrement = SeekButtonIncrement.Ten,
+        seekBackButtonEnabled = true,
+        onSeekForwardButtonClick = {},
+        seekForwardButtonIncrement = SeekButtonIncrement.Thirty,
+        seekForwardButtonEnabled = true,
+    )
+}
+
+@Preview("Disabled - Not playing - Without progress")
+@Composable
+fun PodcastControlButtonsPreviewNoProgress() {
+    PodcastControlButtons(
+        onPlayButtonClick = {},
+        onPauseButtonClick = {},
+        playPauseButtonEnabled = false,
+        playing = false,
+        onSeekBackButtonClick = {},
+        seekBackButtonIncrement = SeekButtonIncrement.Unknown,
+        seekBackButtonEnabled = false,
+        onSeekForwardButtonClick = {},
+        seekForwardButtonIncrement = SeekButtonIncrement.Five,
+        seekForwardButtonEnabled = false,
+    )
+}

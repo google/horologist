4f0120526038c8ec784d323e223393be7d1236fe
Yuri Schimke <yschimke@google.com>
2022-05-12 11:09:53 +0100

    Add a compose testing module and multiepl device preview.

--- compose-testing/src/debug/java/com/google/android/horologist/compose/testing/WearPreviewDevicesPreview.kt
+++ compose-testing/src/debug/java/com/google/android/horologist/compose/testing/WearPreviewDevicesPreview.kt
@@ -14,27 +14,21 @@
  * limitations under the License.
  */
 
-package com.google.android.horologist.compose.layout
+package com.google.android.horologist.compose.testing
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.wear.compose.material.Text
-import com.google.android.horologist.compose.testing.WearPreviewDevices
 
-@WearPreviewDevices
 @Composable
+@Preview
 fun Preview() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.fillMaxRectangle().background(Color.Blue)) {
-            Text("Top")
-            Spacer(modifier = Modifier.weight(1f))
-            Text("Bottom")
-        }
+    Box(modifier =  Modifier.background(Color.LightGray), contentAlignment = Alignment.Center) {
+        Text("Preview")
     }
 }

da1519bbd0df3e848ac560e7e4a338e9f6f86432
Yuri Schimke <yuri@schimke.ee>
2022-05-13 21:46:00 +0100

    Theme previews

--- compose-testing/src/main/java/com/google/android/horologist/compose/testing/WearPreviewDevices.kt
+++ compose-testing/src/main/java/com/google/android/horologist/compose/testing/WearPreviewDevices.kt
@@ -41,3 +41,30 @@ import androidx.compose.ui.tooling.preview.Preview
     group = "Devices - Square",
 )
 public annotation class WearPreviewDevices
+
+@Preview(
+    device = Devices.WEAR_OS_SMALL_ROUND,
+    showSystemUi = true,
+    backgroundColor = 0xff000000,
+    showBackground = true,
+    group = "Devices - Small Round",
+)
+public annotation class WearSmallRoundDevicePreview
+
+@Preview(
+    device = Devices.WEAR_OS_LARGE_ROUND,
+    showSystemUi = true,
+    backgroundColor = 0xff000000,
+    showBackground = true,
+    group = "Devices - Large Round",
+)
+public annotation class WearLargeRoundDevicePreview
+
+@Preview(
+    device = Devices.WEAR_OS_SQUARE,
+    showSystemUi = true,
+    backgroundColor = 0xff000000,
+    showBackground = true,
+    group = "Devices - Square",
+)
+public annotation class WearSquareDevicePreview

6fd0a6eee690929ddd10063ccc0fbacef1e902bd
Yuri Schimke <yuri@schimke.ee>
2022-05-16 10:41:36 +0100

    Fix

--- network-awareness/src/main/java/com/google/android/horologist/networks/status/HighBandwidthRequester.kt
+++ network-awareness/src/main/java/com/google/android/horologist/networks/status/HighBandwidthRequester.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022 Google Inc. All rights reserved.
+ * Copyright 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,7 +29,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
 import java.io.Closeable
-import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine

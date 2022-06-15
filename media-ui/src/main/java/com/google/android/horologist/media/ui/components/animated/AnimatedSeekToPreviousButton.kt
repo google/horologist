53985236a59b906c585236779c88505a98d01b73
Yuri Schimke <yschimke@google.com>
2022-06-15 13:19:55 +0100

    Fix tests

--- media-ui/src/main/java/com/google/android/horologist/media/ui/components/animated/AnimatedSeekToPreviousButton.kt
+++ media-ui/src/main/java/com/google/android/horologist/media/ui/components/animated/AnimatedSeekToPreviousButton.kt
@@ -14,29 +14,13 @@
  * limitations under the License.
  */
 
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.android.horologist.media.ui.components.animated
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
 import androidx.wear.compose.material.ButtonColors
 import androidx.wear.compose.material.ButtonDefaults
@@ -59,7 +43,7 @@ public fun AnimatedSeekToPreviousButton(
     )
     val lottieAnimatable = rememberLottieAnimatable()
 
-    Box(modifier = Modifier.scale(scaleX = -1f, scaleY = 1f)) {
+    Box(modifier = Modifier.graphicsLayer(scaleX = -1f)) {
         AnimatedMediaButton(
             modifier = modifier,
             onClick = onClick,

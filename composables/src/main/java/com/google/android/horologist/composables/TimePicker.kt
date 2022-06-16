8c46a703afcf1478661b32ad5037c4e46e8a7758
Kseniia Shumelchyk <kshum@google.com>
2022-06-16 12:07:33 +0100

    Apply spotless

--- composables/src/main/java/com/google/android/horologist/composables/TimePicker.kt
+++ composables/src/main/java/com/google/android/horologist/composables/TimePicker.kt
@@ -30,7 +30,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize

a3eb7fe71511e2c89be1dfdca769e95483fdf0f9
Yuri Schimke <yschimke@google.com>
2022-06-20 20:40:03 +0100

    Coroutines Media Callback

---
+++ media3-backend/src/main/java/com/google/android/horologist/media3/service/CoroutinesMediaLibrarySessionCallback.kt
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.media3.service;
+
+import androidx.media3.common.MediaItem
+import androidx.media3.session.LibraryResult
+import androidx.media3.session.MediaLibraryService
+import androidx.media3.session.MediaSession
+import com.google.android.horologist.media3.ExperimentalHorologistMedia3BackendApi
+import com.google.android.horologist.media3.logging.ErrorReporter
+import com.google.common.collect.ImmutableList
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.guava.future
+
+/**
+ * ListenableFuture to Coroutines adapting base class for MediaLibrarySession.Callback.
+ *
+ * Each metho is implemented like for like,
+ */
+@ExperimentalHorologistMedia3BackendApi
+abstract class CoroutinesMediaLibrarySessionCallback(
+    val serviceScope: CoroutineScope,
+    val appEventLogger: ErrorReporter
+) :
+    MediaLibraryService.MediaLibrarySession.Callback {
+    override fun onGetLibraryRoot(
+        session: MediaLibraryService.MediaLibrarySession,
+        browser: MediaSession.ControllerInfo,
+        params: MediaLibraryService.LibraryParams?
+    ): ListenableFuture<LibraryResult<MediaItem>> {
+        return serviceScope.future {
+            try {
+                onGetLibraryRootInternal(session, browser, params)
+            } catch (e: Exception) {
+                appEventLogger.logMessage(
+                    "onGetLibraryRoot: $e",
+                    ErrorReporter.Category.App,
+                    ErrorReporter.Level.Error
+                )
+                LibraryResult.ofError(LibraryResult.RESULT_ERROR_BAD_VALUE)
+            }
+        }
+    }
+
+    abstract suspend fun onGetLibraryRootInternal(
+        session: MediaLibraryService.MediaLibrarySession,
+        browser: MediaSession.ControllerInfo,
+        params: MediaLibraryService.LibraryParams?
+    ): LibraryResult<MediaItem>
+
+    override fun onGetItem(
+        session: MediaLibraryService.MediaLibrarySession,
+        browser: MediaSession.ControllerInfo,
+        mediaId: String
+    ): ListenableFuture<LibraryResult<MediaItem>> {
+        return serviceScope.future {
+            try {
+                onGetItemInternal(session, browser, mediaId)
+            } catch (e: Exception) {
+                appEventLogger.logMessage(
+                    "onGetItem: $e",
+                    ErrorReporter.Category.App,
+                    ErrorReporter.Level.Error
+                )
+                LibraryResult.ofError(LibraryResult.RESULT_ERROR_BAD_VALUE)
+            }
+        }
+    }
+
+    abstract suspend fun onGetItemInternal(
+        session: MediaLibraryService.MediaLibrarySession,
+        browser: MediaSession.ControllerInfo,
+        mediaId: String
+    ): LibraryResult<MediaItem>
+
+    override fun onAddMediaItems(
+        mediaSession: MediaSession,
+        controller: MediaSession.ControllerInfo,
+        mediaItems: MutableList<MediaItem>
+    ): ListenableFuture<MutableList<MediaItem>> {
+        return serviceScope.future {
+            onAddMediaItemsInternal(mediaSession, controller, mediaItems)
+        }
+    }
+
+    /**
+     * default implementation of onAddMediaItems that sets the URI from the requestMetadata
+     * if present.
+     */
+    open suspend fun onAddMediaItemsInternal(
+        mediaSession: MediaSession,
+        controller: MediaSession.ControllerInfo,
+        mediaItems: MutableList<MediaItem>
+    ): MutableList<MediaItem> {
+        return mediaItems.map {
+            if (it.requestMetadata.mediaUri != null) {
+                it.buildUpon()
+                    .setUri(it.requestMetadata.mediaUri)
+                    .build()
+            } else {
+                it
+            }
+        }.toMutableList()
+    }
+
+    override fun onGetChildren(
+        session: MediaLibraryService.MediaLibrarySession,
+        browser: MediaSession.ControllerInfo,
+        parentId: String,
+        page: Int,
+        pageSize: Int,
+        params: MediaLibraryService.LibraryParams?
+    ): ListenableFuture<LibraryResult<ImmutableList<MediaItem>>> {
+        return serviceScope.future {
+            try {
+                onGetChildrenInternal(session, browser, parentId, page, pageSize, params)
+            } catch (e: Exception) {
+                appEventLogger.logMessage(
+                    "onGetChildren: $e",
+                    ErrorReporter.Category.App,
+                    ErrorReporter.Level.Error
+                )
+                LibraryResult.ofError(LibraryResult.RESULT_ERROR_BAD_VALUE)
+            }
+        }
+    }
+
+    abstract suspend fun onGetChildrenInternal(
+        session: MediaLibraryService.MediaLibrarySession,
+        browser: MediaSession.ControllerInfo,
+        parentId: String,
+        page: Int,
+        pageSize: Int,
+        params: MediaLibraryService.LibraryParams?
+    ): LibraryResult<ImmutableList<MediaItem>>
+}
+

8aba02a9d56939db768818b58b09cbdee11c138c
Yuri Schimke <yschimke@google.com>
2022-06-22 08:47:27 +0100

    Media player sample

--- media-sample/src/main/java/com/google/android/horologist/mediasample/ui/UampTheme.kt
+++ media-sample/src/main/java/com/google/android/horologist/mediasample/ui/UampTheme.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.android.horologist.mediasample.media
+package com.google.android.horologist.mediasample.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color

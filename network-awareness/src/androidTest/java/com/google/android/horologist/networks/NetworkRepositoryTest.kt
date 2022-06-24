cad0016d0797556619cbaa3656f3286771cfd378
Yuri Schimke <yuri@schimke.ee>
2022-06-24 13:39:36 +0100

    Fix removal of networks. (#273)

---
+++ network-awareness/src/androidTest/java/com/google/android/horologist/networks/NetworkRepositoryTest.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(ExperimentalCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+
+package com.google.android.horologist.networks
+
+import android.content.Context
+import android.net.ConnectivityManager
+import androidx.activity.ComponentActivity
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.horologist.networks.logging.NetworkStatusLogger
+import com.google.android.horologist.networks.status.NetworkRepository
+import com.google.common.truth.Truth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class NetworkRepositoryTest {
+    private lateinit var networkRepository: NetworkRepository
+    private lateinit var context: Context
+    val scope = TestScope()
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(StandardTestDispatcher(scope.testScheduler))
+
+        context = InstrumentationRegistry.getInstrumentation().context
+        networkRepository =
+            NetworkRepository.fromContext(context, scope, NetworkStatusLogger.Logging)
+    }
+
+    @After
+    fun tearDown() {
+        networkRepository.close()
+
+        Dispatchers.resetMain()
+    }
+
+    @Suppress("DEPRECATION")
+    @Test
+    public fun testNetworks() = scope.runTest {
+        val networks = networkRepository.networkStatus.value
+
+        Truth.assertThat(networks.networks).isNotEmpty()
+
+        val connectivityManager =
+            context.getSystemService(ComponentActivity.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val networkIds = connectivityManager.allNetworks.map { it.toString() }
+        val activeNetworkId = connectivityManager.activeNetwork.toString()
+
+        val map: List<String> = networks.networks.map { it.id }
+        Truth.assertThat(map).containsExactlyElementsIn(networkIds)
+        Truth.assertThat(networks.activeNetwork?.id).isEqualTo(activeNetworkId)
+    }
+}

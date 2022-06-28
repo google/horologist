e697a2418be24e6063f50c51ebdfda00f41bc1a8
Yuri Schimke <yschimke@google.com>
2022-06-28 16:43:26 +0100

    Update UI

--- media-ui/src/main/java/com/google/android/horologist/media/ui/components/LoadingMediaDisplay.kt
+++ media-ui/src/main/java/com/google/android/horologist/media/ui/components/LoadingMediaDisplay.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape

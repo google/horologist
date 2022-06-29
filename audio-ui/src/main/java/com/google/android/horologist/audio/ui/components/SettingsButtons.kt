ea5085bb92400cf76f3340d239c95f5cdafe256e
Yuri Schimke <yschimke@google.com>
2022-06-29 08:10:14 +0100

    Fixes

--- audio-ui/src/main/java/com/google/android/horologist/audio/ui/components/SettingsButtons.kt
+++ audio-ui/src/main/java/com/google/android/horologist/audio/ui/components/SettingsButtons.kt
@@ -19,7 +19,6 @@ package com.google.android.horologist.audio.ui.components
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable

63d4d99cfdee41cf531bc3677d15c9a9705fff2c
Gustavo Pagani <gustavopagani@google.com>
2022-07-05 18:04:09 +0100

    Add SecondaryChip (#317)

---
+++ media-ui/src/main/java/com/google/android/horologist/media/ui/components/base/SecondaryChip.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.media.ui.components.base
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.ChipDefaults
+import androidx.wear.compose.material.Text
+import coil.compose.rememberAsyncImagePainter
+import com.google.android.horologist.media.ui.ExperimentalHorologistMediaUiApi
+
+/**
+ * This composable fulfil the redlines of the following components:
+ * - Secondary standard chip - when [largeIcon] value is `false`;
+ * - Chip with small or large avatar - according to [largeIcon] value;
+ */
+@ExperimentalHorologistMediaUiApi
+@Composable
+internal fun SecondaryChip(
+    primaryLabel: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    secondaryLabel: String? = null,
+    icon: Any? = null,
+    largeIcon: Boolean = false,
+    placeholder: Painter? = null,
+    enabled: Boolean = true,
+) {
+    val hasSecondaryLabel = secondaryLabel != null
+    val hasIcon = icon != null
+
+    val primaryLabelParam: (@Composable RowScope.() -> Unit) =
+        {
+            Text(
+                text = primaryLabel,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = if (hasSecondaryLabel || hasIcon) TextAlign.Left else TextAlign.Center,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = if (hasSecondaryLabel) 1 else 2,
+            )
+        }
+
+    val secondaryLabelParam: (@Composable RowScope.() -> Unit)? =
+        secondaryLabel?.let {
+            {
+                Text(
+                    text = secondaryLabel,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                )
+            }
+        }
+
+    val iconParam: (@Composable BoxScope.() -> Unit)? =
+        icon?.let {
+            {
+                val iconSize = if (largeIcon) {
+                    ChipDefaults.LargeIconSize
+                } else {
+                    ChipDefaults.IconSize
+                }
+
+                Row {
+                    if (!largeIcon) {
+                        Spacer(modifier = Modifier.width(4.dp))
+                    }
+
+                    Image(
+                        painter = rememberAsyncImagePainter(
+                            model = icon,
+                            placeholder = placeholder
+                        ),
+                        contentDescription = null, // hidden from talkback
+                        modifier = Modifier.size(iconSize),
+                        contentScale = ContentScale.Fit
+                    )
+                }
+            }
+        }
+
+    Chip(
+        label = primaryLabelParam,
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(),
+        secondaryLabel = secondaryLabelParam,
+        icon = iconParam,
+        colors = ChipDefaults.secondaryChipColors(),
+        enabled = enabled,
+    )
+}
---
+++ media-ui/src/test/java/com/google/android/horologist/media/ui/components/base/SecondaryChipTest.kt
@@ -0,0 +1,313 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(ExperimentalHorologistMediaUiApi::class)
+
+package com.google.android.horologist.media.ui.components.base
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.materialPath
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import app.cash.paparazzi.Paparazzi
+import com.google.android.horologist.media.ui.ExperimentalHorologistMediaUiApi
+import com.google.android.horologist.media.ui.utils.rememberVectorPainter
+import com.google.android.horologist.paparazzi.GALAXY_WATCH4_CLASSIC_LARGE
+import com.google.android.horologist.paparazzi.WearSnapshotHandler
+import com.google.android.horologist.paparazzi.determineHandler
+import org.junit.Rule
+import org.junit.Test
+
+class SecondaryChipTest {
+
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = GALAXY_WATCH4_CLASSIC_LARGE,
+        theme = "android:ThemeOverlay.Material.Dark",
+        maxPercentDifference = 0.0,
+        snapshotHandler = WearSnapshotHandler(determineHandler(0.1))
+    )
+
+    @Test
+    fun withPrimaryLabel() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(primaryLabel = "Primary label", onClick = { })
+            }
+        }
+    }
+
+    @Test
+    fun withSecondaryLabel() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label",
+                    onClick = { },
+                    secondaryLabel = "Secondary label",
+                )
+            }
+        }
+    }
+
+    @Test
+    fun withIcon() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label",
+                    onClick = { },
+                    icon = "iconUri",
+                    placeholder = rememberVectorPainter(
+                        image = Icons.Default.Add,
+                        tintColor = Color.White,
+                    )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun withLargeIcon() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label",
+                    onClick = { },
+                    icon = "iconUri",
+                    largeIcon = true,
+                    placeholder = rememberVectorPainter(
+                        image = Icon32dp,
+                        tintColor = Color.White,
+                    )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun withSecondaryLabelAndIcon() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label",
+                    onClick = { },
+                    secondaryLabel = "Secondary label",
+                    icon = "iconUri",
+                    placeholder = rememberVectorPainter(
+                        image = Icons.Default.Add,
+                        tintColor = Color.White,
+                    )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun withSecondaryLabelAndLargeIcon() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label",
+                    onClick = { },
+                    secondaryLabel = "Secondary label",
+                    icon = "iconUri",
+                    largeIcon = true,
+                    placeholder = rememberVectorPainter(
+                        image = Icon32dp,
+                        tintColor = Color.White,
+                    )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun disabled() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label",
+                    onClick = { },
+                    secondaryLabel = "Secondary label",
+                    icon = "iconUri",
+                    placeholder = rememberVectorPainter(
+                        image = Icons.Default.Add,
+                        tintColor = Color.White,
+                    ),
+                    enabled = false,
+                )
+            }
+        }
+    }
+
+    @Test
+    fun withLongText() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label very very very very very very very very very very very very very very very very very long text",
+                    onClick = { }
+                )
+            }
+        }
+    }
+
+    @Test
+    fun withSecondaryLabelAndLongText() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label very very very very very very very very long text",
+                    onClick = { },
+                    secondaryLabel = "Secondary label very very very very very very very very very long text",
+                    icon = "iconUri",
+                    placeholder = rememberVectorPainter(
+                        image = Icons.Default.Add,
+                        tintColor = Color.White,
+                    ),
+                )
+            }
+        }
+    }
+
+    @Test
+    fun usingSmallIcon() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label",
+                    onClick = { },
+                    icon = "iconUri",
+                    placeholder = rememberVectorPainter(
+                        image = Icon12dp,
+                        tintColor = Color.White,
+                    )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun withLargeIconUsingSmallIcon() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label",
+                    onClick = { },
+                    icon = "iconUri",
+                    largeIcon = true,
+                    placeholder = rememberVectorPainter(
+                        image = Icon12dp,
+                        tintColor = Color.White,
+                    )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun usingExtraLargeIcon() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label",
+                    onClick = { },
+                    icon = "iconUri",
+                    placeholder = rememberVectorPainter(
+                        image = Icon48dp,
+                        tintColor = Color.White,
+                    )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun withLargeIconUsingExtraLargeIcon() {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.background(Color.Black), contentAlignment = Alignment.Center) {
+                SecondaryChip(
+                    primaryLabel = "Primary label",
+                    onClick = { },
+                    icon = "iconUri",
+                    largeIcon = true,
+                    placeholder = rememberVectorPainter(
+                        image = Icon48dp,
+                        tintColor = Color.White,
+                    )
+                )
+            }
+        }
+    }
+
+    companion object {
+
+        private val Icon12dp: ImageVector
+            get() = ImageVector.Builder(
+                name = "Icon Small",
+                defaultWidth = 12f.dp,
+                defaultHeight = 12f.dp,
+                viewportWidth = 12f,
+                viewportHeight = 12f,
+            )
+                .materialPath {
+                    horizontalLineToRelative(12.0f)
+                    verticalLineToRelative(12.0f)
+                    horizontalLineTo(0.0f)
+                    close()
+                }
+                .build()
+
+        private val Icon32dp: ImageVector
+            get() = ImageVector.Builder(
+                name = "Icon Large",
+                defaultWidth = 32f.dp,
+                defaultHeight = 32f.dp,
+                viewportWidth = 32f,
+                viewportHeight = 32f,
+            )
+                .materialPath {
+                    horizontalLineToRelative(32.0f)
+                    verticalLineToRelative(32.0f)
+                    horizontalLineTo(0.0f)
+                    close()
+                }
+                .build()
+
+        private val Icon48dp: ImageVector
+            get() = ImageVector.Builder(
+                name = "Icon Extra Large",
+                defaultWidth = 48f.dp,
+                defaultHeight = 48f.dp,
+                viewportWidth = 48f,
+                viewportHeight = 48f,
+            )
+                .materialPath {
+                    horizontalLineToRelative(48.0f)
+                    verticalLineToRelative(48.0f)
+                    horizontalLineTo(0.0f)
+                    close()
+                }
+                .build()
+    }
+}

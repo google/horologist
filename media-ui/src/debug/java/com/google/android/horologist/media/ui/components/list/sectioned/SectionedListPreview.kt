6047570ed83d282f9dabd46768d197dde0e2521d
Gustavo Pagani <gustavopagani@google.com>
2022-08-11 15:33:07 +0100

    Add SectionedList

---
+++ media-ui/src/debug/java/com/google/android/horologist/media/ui/components/list/sectioned/SectionedListPreview.kt
@@ -0,0 +1,241 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(ExperimentalHorologistMediaUiApi::class)
+
+package com.google.android.horologist.media.ui.components.list.sectioned
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FeaturedPlayList
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.rememberScalingLazyListState
+import com.google.android.horologist.compose.tools.WearPreviewDevices
+import com.google.android.horologist.media.ui.ExperimentalHorologistMediaUiApi
+import com.google.android.horologist.media.ui.components.base.SecondaryPlaceholderChip
+import com.google.android.horologist.media.ui.components.base.StandardChip
+import com.google.android.horologist.media.ui.components.base.StandardChipType
+import com.google.android.horologist.media.ui.components.base.Title
+import com.google.android.horologist.media.ui.utils.rememberVectorPainter
+
+@WearPreviewDevices
+@Composable
+fun SectionedListPreviewLoadingSection() {
+    SectionedList(
+        focusRequester = FocusRequester(),
+        scalingLazyListState = rememberScalingLazyListState()
+    ) {
+        downloadsSection(scope = this, state = Section.State.Loading)
+
+        favouritesSection(scope = this, state = Section.State.Empty)
+    }
+}
+
+@WearPreviewDevices
+@Composable
+fun SectionedListPreviewLoadedSection() {
+    SectionedList(
+        focusRequester = FocusRequester(),
+        scalingLazyListState = rememberScalingLazyListState()
+    ) {
+        downloadsSection(scope = this, state = Section.State.Loaded(downloads))
+
+        favouritesSection(scope = this, state = Section.State.Failed)
+    }
+}
+
+@WearPreviewDevices
+@Composable
+fun SectionedListPreviewFailedSection() {
+    SectionedList(
+        focusRequester = FocusRequester(),
+        scalingLazyListState = rememberScalingLazyListState()
+    ) {
+        downloadsSection(scope = this, state = Section.State.Failed)
+
+        favouritesSection(scope = this, state = Section.State.Loaded(favourites))
+    }
+}
+
+@WearPreviewDevices
+@Composable
+fun SectionedListPreviewEmptySection() {
+    SectionedList(
+        focusRequester = FocusRequester(),
+        scalingLazyListState = rememberScalingLazyListState()
+    ) {
+        downloadsSection(scope = this, state = Section.State.Empty)
+
+        favouritesSection(scope = this, state = Section.State.Loading)
+    }
+}
+
+private val downloads = listOf("Nu Metal Essentials", "00s Rock")
+
+private fun downloadsSection(scope: SectionedListScope, state: Section.State) {
+    scope.section(state = state) {
+        header { DownloadsHeader() }
+
+        loading { DownloadsLoading() }
+
+        loaded { DownloadsLoaded(it) }
+
+        failed { DownloadsFailed() }
+
+        empty { DownloadsEmpty() }
+
+        footer { DownloadsFooter() }
+    }
+}
+
+@Composable
+private fun DownloadsHeader() {
+    Title(
+        text = "Downloads",
+        modifier = Modifier.padding(bottom = 12.dp)
+    )
+}
+
+@Composable
+private fun DownloadsLoading() {
+    SecondaryPlaceholderChip()
+}
+
+@Composable
+private fun DownloadsLoaded(label: String) {
+    StandardChip(
+        label = label,
+        onClick = { },
+        icon = "icon",
+        largeIcon = true,
+        placeholder = rememberVectorPainter(
+            image = Icons.Default.FeaturedPlayList,
+            tintColor = Color.Green
+        ),
+        chipType = StandardChipType.Secondary
+    )
+}
+
+@Composable
+private fun DownloadsFailed() {
+    Text(
+        text = "Failed to load downloads. Please try again later.",
+        modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.body2
+    )
+}
+
+@Composable
+private fun DownloadsEmpty() {
+    Text(
+        text = "Download music to start listening.",
+        modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.body2
+    )
+}
+
+@Composable
+private fun DownloadsFooter() {
+    StandardChip(
+        label = "More downloads..",
+        onClick = { },
+        chipType = StandardChipType.Secondary
+    )
+}
+
+private val favourites = listOf("Dance Anthems", "Indie Jukebox")
+
+private fun favouritesSection(scope: SectionedListScope, state: Section.State) {
+    scope.section(state = state) {
+        header { FavouritesHeader() }
+
+        loading { FavouritesLoading() }
+
+        loaded { FavouritesLoaded(it) }
+
+        failed { FavouritesFailed() }
+
+        empty { FavouritesEmpty() }
+
+        footer { FavouritesFooter() }
+    }
+}
+
+@Composable
+private fun FavouritesHeader() {
+    Title(
+        text = "Favourites",
+        modifier = Modifier.padding(top = 12.dp, bottom = 12.dp)
+    )
+}
+
+@Composable
+private fun FavouritesLoading() {
+    SecondaryPlaceholderChip()
+}
+
+@Composable
+private fun FavouritesLoaded(label: String) {
+    StandardChip(
+        label = label,
+        onClick = { },
+        icon = "icon",
+        largeIcon = true,
+        placeholder = rememberVectorPainter(
+            image = Icons.Default.FeaturedPlayList,
+            tintColor = Color.Green
+        ),
+        chipType = StandardChipType.Secondary
+    )
+}
+
+@Composable
+private fun FavouritesFailed() {
+    Text(
+        text = "Failed to load favourites. Please try again later.",
+        modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.body2
+    )
+}
+
+@Composable
+private fun FavouritesEmpty() {
+    Text(
+        text = "Mark songs or albums as favourites to see them here.",
+        modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.body2
+    )
+}
+
+@Composable
+fun FavouritesFooter() {
+    StandardChip(
+        label = "More favourites..",
+        onClick = { },
+        chipType = StandardChipType.Secondary
+    )
+}
---
+++ media-ui/src/main/java/com/google/android/horologist/media/ui/components/list/sectioned/SectionedList.kt
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.media.ui.components.list.sectioned
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.wear.compose.material.ScalingLazyColumn
+import androidx.wear.compose.material.ScalingLazyListScope
+import androidx.wear.compose.material.ScalingLazyListState
+import com.google.android.horologist.compose.navscaffold.scrollableColumn
+import com.google.android.horologist.media.ui.ExperimentalHorologistMediaUiApi
+
+/**
+ * A list component that is split into [sections][Section].
+ * Each [Section] has its own [state][Section.State] controlled individually.
+ */
+@ExperimentalHorologistMediaUiApi
+@Composable
+public fun SectionedList(
+    focusRequester: FocusRequester,
+    scalingLazyListState: ScalingLazyListState,
+    modifier: Modifier = Modifier,
+    scope: SectionedListScope.() -> Unit
+) {
+    ScalingLazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .scrollableColumn(focusRequester, scalingLazyListState),
+        state = scalingLazyListState
+    ) {
+        SectionedListScope().apply(scope).sections.forEach { section ->
+            section.display(this)
+        }
+    }
+}
+
+internal fun <T> Section<T>.display(scope: ScalingLazyListScope) {
+    val section = this
+    section.headerContent?.let { content ->
+        scope.item { content() }
+    }
+
+    when (section.state) {
+        Section.State.Loading -> {
+            section.loadingContent?.let { content ->
+                scope.item { content() }
+            }
+        }
+        is Section.State.Loaded<*> -> {
+            val list = section.state.list
+            scope.items(list.size) { index ->
+                @Suppress("UNCHECKED_CAST")
+                section.loadedContent(list[index] as T)
+            }
+        }
+        Section.State.Failed -> {
+            section.failedContent?.let { content ->
+                scope.item { content() }
+            }
+        }
+        Section.State.Empty -> {
+            section.emptyContent?.let { content ->
+                scope.item { content() }
+            }
+        }
+    }
+
+    section.footerContent?.let { content ->
+        if (!displayFooterOnlyOnLoadedState || state is Section.State.Loaded<*>) {
+            scope.item { content() }
+        }
+    }
+}
+
+/**
+ * A section in [SectionedList].
+ */
+public data class Section<T> internal constructor(
+    val state: State,
+    val headerContent: (@Composable () -> Unit)? = null,
+    val loadingContent: (@Composable () -> Unit)? = null,
+    val loadedContent: @Composable (T) -> Unit,
+    val failedContent: (@Composable () -> Unit)? = null,
+    val emptyContent: (@Composable () -> Unit)? = null,
+    val footerContent: (@Composable () -> Unit)? = null,
+    val displayFooterOnlyOnLoadedState: Boolean = true
+) {
+    /**
+     * A state of a [Section].
+     */
+    public sealed class State {
+        public object Loading : State()
+
+        public data class Loaded<T>(
+            val list: List<T>
+        ) : State()
+
+        public object Failed : State()
+
+        public object Empty : State()
+    }
+}
+
+/**
+ * Receiver scope which is used by [SectionedList].
+ */
+public class SectionedListScope {
+
+    internal val sections: MutableList<Section<*>> = mutableListOf()
+
+    public fun <T> section(
+        state: Section.State,
+        displayFooterOnlyOnLoadedState: Boolean = true,
+        builder: SectionContentScope<T>.() -> Unit
+    ) {
+        with(SectionContentScope<T>().apply(builder)) {
+            sections.add(
+                Section(
+                    state = state,
+                    headerContent = this.headerContent,
+                    loadingContent = this.loadingContent,
+                    loadedContent = this.loadedContent,
+                    failedContent = this.failedContent,
+                    emptyContent = this.emptyContent,
+                    footerContent = this.footerContent,
+                    displayFooterOnlyOnLoadedState = displayFooterOnlyOnLoadedState
+                )
+            )
+        }
+    }
+
+    /**
+     * Add a section in [loaded][Section.State.Loaded] state.
+     */
+    public fun <T> section(
+        list: List<T>,
+        builder: SectionContentScope<T>.() -> Unit
+    ): Unit = section(
+        state = Section.State.Loaded(list),
+        displayFooterOnlyOnLoadedState = true,
+        builder = builder
+    )
+
+    /**
+     * Add a section in [loaded][Section.State.Loaded] state with a single item.
+     */
+    public fun section(
+        builder: SectionContentScope<Unit>.() -> Unit
+    ): Unit = section(
+        state = Section.State.Loaded(listOf(Unit)),
+        displayFooterOnlyOnLoadedState = true,
+        builder = builder
+    )
+}
+
+/**
+ * Receiver scope which is used by [SectionedListScope].
+ */
+public class SectionContentScope<T> {
+
+    internal var headerContent: @Composable () -> Unit = { }
+        private set
+
+    internal var loadingContent: @Composable () -> Unit = { }
+        private set
+
+    internal var loadedContent: @Composable (T) -> Unit = { }
+        private set
+
+    internal var failedContent: @Composable () -> Unit = { }
+        private set
+
+    internal var emptyContent: @Composable () -> Unit = { }
+        private set
+
+    internal var footerContent: @Composable () -> Unit = { }
+        private set
+
+    public fun header(content: @Composable () -> Unit) {
+        headerContent = content
+    }
+
+    public fun loading(content: @Composable () -> Unit) {
+        loadingContent = content
+    }
+
+    public fun loaded(content: @Composable (T) -> Unit) {
+        loadedContent = content
+    }
+
+    public fun failed(content: @Composable () -> Unit) {
+        failedContent = content
+    }
+
+    public fun empty(content: @Composable () -> Unit) {
+        emptyContent = content
+    }
+
+    public fun footer(content: @Composable () -> Unit) {
+        footerContent = content
+    }
+}

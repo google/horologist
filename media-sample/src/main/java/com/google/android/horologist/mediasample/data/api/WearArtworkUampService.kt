6c505deadc2132f8db2c7495c203974c12398ef8
Gustavo Pagani <gustavopagani@google.com>
2022-08-15 12:52:29 +0100

    Add backend folder

--- media-sample/src/main/java/com/google/android/horologist/mediasample/data/api/WearArtworkUampService.kt
+++ media-sample/src/main/java/com/google/android/horologist/mediasample/data/api/WearArtworkUampService.kt
@@ -19,12 +19,13 @@ package com.google.android.horologist.mediasample.data.api
 import com.google.android.horologist.mediasample.data.api.model.CatalogApiModel
 
 /**
- * Decorating [UampService] to use wear approriate image sizes.
+ * Decorating [UampService] to use wear appropriate image sizes.
  */
 class WearArtworkUampService(
     private val uampService: UampService
 ) : UampService {
-    val githubPrefix = "https://media.githubusercontent.com/media/google/horologist/main/media-sample/src/main/res/drawable-mdpi"
+    private val githubPrefix =
+        "https://media.githubusercontent.com/media/google/horologist/main/media-sample/backend/images/drawable-mdpi"
 
     override suspend fun catalog(): CatalogApiModel {
         val originalCatalog = uampService.catalog()
@@ -42,6 +43,9 @@ class WearArtworkUampService(
             "https://storage.googleapis.com/automotive-media/album_art.jpg" -> "$githubPrefix/album_art.jpg"
             "https://storage.googleapis.com/automotive-media/album_art_2.jpg" -> "$githubPrefix/album_art_2.jpg"
             "https://storage.googleapis.com/automotive-media/album_art_3.jpg" -> "$githubPrefix/album_art_3.jpg"
+            "https://storage.googleapis.com/uamp/Spatial Audio/Marching band.jpg" -> "$githubPrefix/marching_band.jpg"
+            "https://storage.googleapis.com/uamp/Spatial Audio/Chickens.jpg" -> "$githubPrefix/chickens.jpg"
+            "https://storage.googleapis.com/uamp/Spatial Audio/Rural market.jpg" -> "$githubPrefix/rural_market.jpg"
             else -> image
         }
     }

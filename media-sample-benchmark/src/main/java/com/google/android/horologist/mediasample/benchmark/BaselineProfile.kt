f4744a0c1cd1cb5d2b7ffb9a79ade1307feebbfb
Yuri Schimke <yuri@schimke.ee>
2022-09-08 16:21:33 +0100

    Fixes

--- media-sample-benchmark/src/main/java/com/google/android/horologist/mediasample/benchmark/BaselineProfile.kt
+++ media-sample-benchmark/src/main/java/com/google/android/horologist/mediasample/benchmark/BaselineProfile.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2021 The Android Open Source Project
+ * Copyright 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,17 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.wearable.composeadvanced.benchmark
+package com.google.android.horologist.mediasample.benchmark
 
 import android.content.Intent
-import android.graphics.Point
 import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.Until
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Rule

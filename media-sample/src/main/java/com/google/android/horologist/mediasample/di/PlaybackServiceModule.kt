e42f5bd4eac5b4995c535098c389e82d304c1a2b
Yuri Schimke <yuri@schimke.ee>
2022-09-15 18:45:33 +0100

    Fix request type for streaming (#581)
    * Fix request type for streaming
    
    * Fix request type for streaming

--- media-sample/src/main/java/com/google/android/horologist/mediasample/di/PlaybackServiceModule.kt
+++ media-sample/src/main/java/com/google/android/horologist/mediasample/di/PlaybackServiceModule.kt
@@ -57,7 +57,7 @@ import com.google.android.horologist.mediasample.data.service.playback.UampMedia
 import com.google.android.horologist.mediasample.domain.SettingsRepository
 import com.google.android.horologist.mediasample.domain.strategy
 import com.google.android.horologist.mediasample.ui.AppConfig
-import com.google.android.horologist.networks.data.RequestType
+import com.google.android.horologist.networks.data.RequestType.MediaRequest.Companion.StreamRequest
 import com.google.android.horologist.networks.okhttp.NetworkAwareCallFactory
 import dagger.Module
 import dagger.Provides
@@ -133,7 +133,7 @@ object PlaybackServiceModule {
         OkHttpDataSource.Factory(
             NetworkAwareCallFactory(
                 callFactory,
-                defaultRequestType = RequestType.UnknownRequest
+                defaultRequestType = StreamRequest
             )
         )
             .setTransferListener(transferListener)

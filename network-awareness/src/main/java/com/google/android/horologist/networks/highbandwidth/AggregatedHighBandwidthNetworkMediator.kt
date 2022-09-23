701642080adc5c09a7c41efe3d3bc04de11251b4
Yuri Schimke <yuri@schimke.ee>
2022-09-23 14:51:44 +0100

    Reduce surface area of NetworkMediator

--- network-awareness/src/main/java/com/google/android/horologist/networks/highbandwidth/AggregatedHighBandwidthNetworkMediator.kt
+++ network-awareness/src/main/java/com/google/android/horologist/networks/highbandwidth/AggregatedHighBandwidthNetworkMediator.kt
@@ -39,7 +39,7 @@ public class AggregatedHighBandwidthNetworkMediator(
 ) : HighBandwidthNetworkMediator {
     @GuardedBy("this")
     private var requests = AggregatedRequests()
-    override val requested: MutableStateFlow<HighBandwidthRequest?> = MutableStateFlow(null)
+    val requested: MutableStateFlow<HighBandwidthRequest?> = MutableStateFlow(null)
     override val pinned: StateFlow<NetworkType?> = networkRequester.pinnedNetwork
 
     override fun requestHighBandwidthNetwork(

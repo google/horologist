f8f93b40685fa509d0a651dd13ea9a5a34c3366a
laiyichin <laiyichin@gmail.com>
2022-09-25 20:12:38 +0100

    Clean-up unused method ToggleSetting from UampSettingsSecreen class. (#621)

--- media-sample/src/main/java/com/google/android/horologist/mediasample/ui/settings/UampSettingsScreen.kt
+++ media-sample/src/main/java/com/google/android/horologist/mediasample/ui/settings/UampSettingsScreen.kt
@@ -189,32 +189,6 @@ fun ActionSetting(
 }
 
 @Composable
-fun ToggleSetting(
-    value: Boolean,
-    text: String,
-    enabled: Boolean = true,
-    onCheckedChange: (Boolean) -> Unit
-) {
-    ToggleChip(
-        checked = value,
-        toggleControl = {
-            Icon(
-                imageVector = ToggleChipDefaults.radioIcon(checked = value),
-                contentDescription = if (value) stringResource(id = R.string.on) else stringResource(
-                    id = R.string.off
-                )
-            )
-        },
-        enabled = enabled,
-        onCheckedChange = onCheckedChange,
-        label = {
-            Text(text)
-        },
-        modifier = Modifier.fillMaxWidth()
-    )
-}
-
-@Composable
 fun CheckedSetting(
     value: Boolean,
     text: String,

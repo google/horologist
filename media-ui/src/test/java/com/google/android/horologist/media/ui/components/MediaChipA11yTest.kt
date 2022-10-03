f86e4293b0f672729f40793e1b295772df166a4a
Gustavo Pagani <gustavopagani@google.com>
2022-10-03 08:24:27 +0100

    Improve snapshot tests (#648)

---
+++ media-ui/src/test/java/com/google/android/horologist/media/ui/components/MediaChipA11yTest.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(
+    ExperimentalHorologistComposeToolsApi::class,
+    ExperimentalHorologistPaparazziApi::class,
+    ExperimentalHorologistMediaUiApi::class
+)
+
+package com.google.android.horologist.media.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import app.cash.paparazzi.Paparazzi
+import com.google.android.horologist.compose.tools.ExperimentalHorologistComposeToolsApi
+import com.google.android.horologist.compose.tools.a11y.ComposeA11yExtension
+import com.google.android.horologist.media.ui.ExperimentalHorologistMediaUiApi
+import com.google.android.horologist.media.ui.state.model.MediaUiModel
+import com.google.android.horologist.paparazzi.ExperimentalHorologistPaparazziApi
+import com.google.android.horologist.paparazzi.GALAXY_WATCH4_CLASSIC_LARGE
+import com.google.android.horologist.paparazzi.a11y.A11ySnapshotHandler
+import com.google.android.horologist.paparazzi.determineHandler
+import org.junit.Rule
+import org.junit.Test
+
+class MediaChipA11yTest {
+    private val maxPercentDifference = 1.0
+
+    private val composeA11yExtension = ComposeA11yExtension()
+
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = GALAXY_WATCH4_CLASSIC_LARGE,
+        theme = "android:ThemeOverlay.Material.Dark",
+        maxPercentDifference = maxPercentDifference,
+        renderExtensions = setOf(composeA11yExtension),
+        snapshotHandler = A11ySnapshotHandler(
+            delegate = determineHandler(
+                maxPercentDifference = maxPercentDifference
+            ),
+            accessibilityStateFn = { composeA11yExtension.accessibilityState }
+        )
+    )
+
+    @Test
+    fun a11y() {
+        paparazzi.snapshot {
+            Box(
+                modifier = Modifier.background(Color.Black),
+                contentAlignment = Alignment.Center
+            ) {
+                MediaChip(
+                    media = MediaUiModel(
+                        id = "id",
+                        title = "Red Hot Chilli Peppers",
+                        artworkUri = "https://example.org/image.png"
+                    ),
+                    onClick = {}
+                )
+            }
+        }
+    }
+}

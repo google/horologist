2a897f9bd61c5b47347e78cfe47b1fbdfba1c72b
Gustavo Pagani <gustavopagani@google.com>
2022-10-24 20:51:53 +0100

    Add fixed version of rememberPickerState and change DatePicker and TimePicker to use it (#698) (#703)

--- composables/src/main/java/com/google/android/horologist/composables/DatePicker.kt
+++ composables/src/main/java/com/google/android/horologist/composables/DatePicker.kt
@@ -49,7 +49,6 @@ import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.PickerState
 import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.rememberPickerState
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.temporal.TemporalAdjusters

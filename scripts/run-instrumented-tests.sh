192705229f4763291edaf4cfef656aa46ff2ad63
Yuri Schimke <yuri@schimke.ee>
2022-11-11 16:54:51 +0000

    Fix macrobenchmark and test running in CI (#731)

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -23,6 +23,7 @@ SHARD_INDEX=0
 # By default we don't log
 LOG_FILE=""
 TEST_OPTIONS=""
+MACROBENCHMARK_RULES=""
 
 # Parse parameters
 for i in "$@"; do
@@ -55,6 +56,10 @@ for i in "$@"; do
     BASE_REF="${i#*=}"
     shift
     ;;
+  --macrobenchmark-rules=*)
+    MACROBENCHMARK_RULES="${i#*=}"
+    shift
+    ;;
   *)
     echo "Unknown option"
     exit 1
@@ -79,6 +84,11 @@ FILTER_OPTS=""
 if [[ -z "$RUN_FLAKY_TESTS" ]]; then
   FILTER_OPTS="$FILTER_OPTS -Pandroid.testInstrumentationRunnerArguments.notAnnotation=androidx.test.filters.FlakyTest"
 fi
+
+if [[ ! -z "$MACROBENCHMARK_RULES" ]]; then
+  FILTER_OPTS="$FILTER_OPTS -Pandroid.testInstrumentationRunnerArguments.androidx.benchmark.enabledRules=$MACROBENCHMARK_RULES"
+fi
+
 
 # If we're set to only run affected test, update the Gradle task
 if [[ ! -z "$RUN_AFFECTED" ]]; then

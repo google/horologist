ee888d6ca8aaa470b22ccd67d863ef7d561e200a
Yuri Schimke <yuri@schimke.ee>
2022-12-07 09:00:27 +0000

    Revert

--- sample/src/main/java/com/google/android/horologist/scratch/ScratchActivity.kt
+++ sample/src/main/java/com/google/android/horologist/scratch/ScratchActivity.kt
@@ -19,79 +19,172 @@ package com.google.android.horologist.scratch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.VolumeDown
-import androidx.compose.material.icons.filled.VolumeUp
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.AutoCenteringParams
+import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.PositionIndicator
 import androidx.wear.compose.material.Scaffold
-import androidx.wear.compose.material.Stepper
+import androidx.wear.compose.material.ScalingLazyColumn
+import androidx.wear.compose.material.ScalingLazyListAnchorType
 import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TimeSource
 import androidx.wear.compose.material.TimeText
-import com.google.android.horologist.compose.tools.WearSquareDevicePreview
+import androidx.wear.compose.material.curvedText
+import androidx.wear.compose.material.rememberScalingLazyListState
+import androidx.wear.compose.material.scrollAway
 
 class ScratchActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            VolumeScreen()
+            WearApp()
         }
     }
 }
 
-@WearSquareDevicePreview
+data class Offsets(
+    val index: Int = 0,
+    val offset: Int = 0
+)
+
 @Composable
-fun VolumeScreen() {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        timeText = {
-            TimeText(
-                timeSource = object : TimeSource {
-                    override val currentTime: String
-                        @Composable
-                        get() = "11:02 AM"
+fun WearApp() {
+    var initialOffsetsMode by remember { mutableStateOf(0) }
+    val initialOffsets = remember {
+        listOf(
+            Offsets(0, 0),
+            Offsets(1, 0),
+            Offsets(1, -20),
+            Offsets(1, 20),
+            Offsets(2, 0)
+        )
+    }
+
+    var autoCenteringMode by remember { mutableStateOf(1) }
+    val autoCenterings = remember {
+        listOf(
+            Pair("null", null),
+            Pair("0/0", AutoCenteringParams(0, 0)),
+            Pair("1/0", AutoCenteringParams(1, 0)),
+            Pair("2/0", AutoCenteringParams(2, 0)),
+            Pair("3/0", AutoCenteringParams(3, 0))
+        )
+    }
+
+    var itemHeightMode by remember { mutableStateOf(0) }
+    val itemHeights = remember { listOf(40, 80, 120) }
+
+    var anchorTypeMode by remember { mutableStateOf(0) }
+    val anchorTypes = remember {
+        listOf(
+            Pair("Center", ScalingLazyListAnchorType.ItemCenter),
+            Pair("Start", ScalingLazyListAnchorType.ItemStart)
+        )
+    }
+
+    key(initialOffsetsMode) {
+        val initialOffset = initialOffsets[initialOffsetsMode]
+        val itemHeight = itemHeights[itemHeightMode]
+        val autoCentering = autoCenterings[autoCenteringMode]
+        val anchorType = anchorTypes[anchorTypeMode]
+
+        val listState = rememberScalingLazyListState(
+            initialCenterItemIndex = initialOffset.index,
+            initialCenterItemScrollOffset = initialOffset.offset
+        )
+
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            timeText = {
+                TimeText(
+                    modifier = Modifier
+                        .scrollAway(
+                            listState,
+                            initialOffset.index,
+                            initialOffset.offset
+                                .dp
+                        ),
+                    startCurvedContent = {
+                        curvedText("${listState.centerItemIndex}/${listState.centerItemScrollOffset}")
+                    }
+                )
+            }
+        ) {
+            ScalingLazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                state = listState,
+                autoCentering = autoCentering.second,
+                anchorType = anchorType.second
+            ) {
+                item {
+                    val text = "Initial Offset: ${initialOffset.index} / ${initialOffset.offset}"
+                    FixedHeightChip(text, itemHeight, onClick = {
+                        initialOffsetsMode = (initialOffsetsMode + 1) % initialOffsets.size
+                    })
                 }
-            )
-        },
-        positionIndicator = {
-            PositionIndicator(value = { 0.5f })
+                item {
+                    val text = "Auto Centering: ${autoCentering.first}"
+                    FixedHeightChip(text, itemHeight, onClick = {
+                        println(autoCenteringMode)
+                        autoCenteringMode = (autoCenteringMode + 1) % autoCenterings.size
+                    })
+                }
+                item {
+                    val text = "Anchor Type: ${anchorType.first}"
+                    FixedHeightChip(text, itemHeight, onClick = {
+                        anchorTypeMode = (anchorTypeMode + 1) % anchorTypes.size
+                    })
+                }
+                item {
+                    val text = "Item Height: $itemHeight"
+                    FixedHeightChip(text, itemHeight, onClick = {
+                        itemHeightMode = (itemHeightMode + 1) % itemHeights.size
+                    })
+                }
+            }
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawLine(
+                    Color.Red,
+                    Offset(0f, size.height / 2f),
+                    Offset(size.width, size.height / 2f)
+                )
+            }
         }
+    }
+}
+
+@Composable
+fun FixedHeightChip(text: String, itemHeight: Int, onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .height(itemHeight.dp)
+            .fillMaxWidth()
+            .border(1.dp, Color.DarkGray)
     ) {
-        Stepper(
-            value = 5,
-            onValueChange = {},
-            valueProgression = 1..10,
-            decreaseIcon = {
-                Icon(
-                    modifier = Modifier
-                        .size(26.dp),
-                    imageVector = Icons.Default.VolumeDown,
-                    contentDescription = "Decrease Volume"
+        Chip(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = onClick,
+            label = {
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.caption3
                 )
-            },
-            increaseIcon = {
-                Icon(
-                    modifier = Modifier
-                        .size(26.dp),
-                    imageVector = Icons.Default.VolumeUp,
-                    contentDescription = "Increase Volume"
-                )
-            }) {
-            Text(
-                "Volume",
-                style = MaterialTheme.typography.button,
-                maxLines = 1,
-                overflow = TextOverflow.Clip
-            )
-        }
+            }
+        )
     }
 }

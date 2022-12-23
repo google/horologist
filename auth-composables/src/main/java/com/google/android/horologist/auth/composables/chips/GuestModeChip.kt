06ef89640d04c06b44bf76e13ab20d49aca94114
Gustavo Pagani <gustavopagani@google.com>
2022-12-23 10:39:18 +0000

    Add snapshot tests for auth composables (#885)

--- auth-composables/src/main/java/com/google/android/horologist/auth/composables/chips/GuestModeChip.kt
+++ auth-composables/src/main/java/com/google/android/horologist/auth/composables/chips/GuestModeChip.kt
@@ -21,11 +21,15 @@ import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.wear.compose.material.Chip
 import com.google.android.horologist.auth.composables.ExperimentalHorologistAuthComposablesApi
 import com.google.android.horologist.auth.composables.R
 import com.google.android.horologist.base.ui.components.StandardChip
 import com.google.android.horologist.base.ui.components.StandardChipType
 
+/**
+ * An opinionated [Chip] to represent the "Guest mode" action.
+ */
 @ExperimentalHorologistAuthComposablesApi
 @Composable
 public fun GuestModeChip(

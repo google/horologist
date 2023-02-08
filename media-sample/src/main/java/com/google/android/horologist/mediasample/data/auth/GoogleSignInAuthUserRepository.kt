515f2ec83451442a31417b318ac582552cff60f7
Yuri Schimke <yuri@schimke.ee>
2023-02-08 13:13:28 +0000

    Avoid StrictMode violation from reading on main thread

--- media-sample/src/main/java/com/google/android/horologist/mediasample/data/auth/GoogleSignInAuthUserRepository.kt
+++ media-sample/src/main/java/com/google/android/horologist/mediasample/data/auth/GoogleSignInAuthUserRepository.kt
@@ -25,11 +25,13 @@ import com.google.android.horologist.auth.data.common.model.AuthUser
 import com.google.android.horologist.auth.data.common.repository.AuthUserRepository
 import com.google.android.horologist.auth.data.googlesignin.AuthUserMapper
 import com.google.android.horologist.auth.data.googlesignin.GoogleSignInEventListener
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 /**
  * An implementation of [AuthUserRepository] for the Google Sign-In authentication method.
@@ -44,8 +46,8 @@ public class GoogleSignInAuthUserRepository(
 
     public val authState: Flow<AuthUser?> = _authTrigger.map { getAuthenticated() }
 
-    override suspend fun getAuthenticated(): AuthUser? {
-        return AuthUserMapper.map(GoogleSignIn.getLastSignedInAccount(applicationContext))
+    override suspend fun getAuthenticated(): AuthUser? = withContext(Dispatchers.Default) {
+        AuthUserMapper.map(GoogleSignIn.getLastSignedInAccount(applicationContext))
     }
 
     override suspend fun onSignedIn(account: GoogleSignInAccount) {
@@ -55,9 +57,6 @@ public class GoogleSignInAuthUserRepository(
     public fun onSignedOut() {
         _authTrigger.update { it + 1 }
     }
-
-    public fun getAuthenticatedGoogleSignInAccount(): GoogleSignInAccount? =
-        GoogleSignIn.getLastSignedInAccount(applicationContext)
 
     public suspend fun signOut() {
         googleSignInClient.signOut().await()

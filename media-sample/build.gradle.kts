703bf2ac3fb362340bb603a959abfc0c0ba87065
Yuri Schimke <yuri@schimke.ee>
2023-03-03 17:17:15 +0000

    Bump dependencies

--- media-sample/build.gradle.kts
+++ media-sample/build.gradle.kts
@@ -263,10 +263,10 @@ dependencies {
     kaptAndroidTest(libs.dagger.hiltandroidcompiler)
 
     constraints {
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0") {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.10") {
             because("kotlin-stdlib-jdk7 is now a part of kotlin-stdlib")
         }
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0") {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10") {
             because("kotlin-stdlib-jdk8 is now a part of kotlin-stdlib")
         }
     }

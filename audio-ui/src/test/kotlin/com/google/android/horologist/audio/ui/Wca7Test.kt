d8a7324d5c04a40ff49a8e837ccb6dee87da6094
Yuri Schimke <yuri@schimke.ee>
2023-03-23 17:45:06 +0000

    Failing test

---
+++ audio-ui/src/test/kotlin/com/google/android/horologist/audio/ui/Wca7Test.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(
+    ExperimentalHorologistApi::class
+)
+
+package com.google.android.horologist.audio.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.wear.compose.material.Text
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import com.google.android.horologist.audio.AudioOutput
+import com.google.android.horologist.audio.VolumeState
+import com.google.android.horologist.compose.tools.ThemeValues
+import com.google.android.horologist.compose.tools.themeValues
+import com.google.android.horologist.paparazzi.WearPaparazzi
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+class Wca7Test() {
+    @get:Rule
+    val paparazzi = WearPaparazzi()
+
+    @Test
+    fun list() {
+        paparazzi.snapshot() {
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .background(Color.DarkGray)) {
+                repeat(100) {
+                    Text(text = "Item $it", modifier = Modifier.align(CenterHorizontally))
+                }
+            }
+        }
+    }
+}

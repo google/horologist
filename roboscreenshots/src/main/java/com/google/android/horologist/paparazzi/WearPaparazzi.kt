89d8d5adf6ad83f90205f4b583b00c79fcd4303e
Yuri Schimke <yuri@schimke.ee>
2023-03-24 11:09:04 +0000

    RNG Test

---
+++ roboscreenshots/src/main/java/com/google/android/horologist/paparazzi/WearPaparazzi.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.paparazzi
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import app.cash.paparazzi.RenderExtension
+import app.cash.paparazzi.SnapshotHandler
+import com.android.ide.common.rendering.api.SessionParams
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+
+private const val DefaultMaxPercentDifference = 0.1
+
+@ExperimentalHorologistApi
+public fun WearPaparazzi(
+    deviceConfig: DeviceConfig = DeviceConfig.GALAXY_WATCH4_CLASSIC_LARGE,
+    theme: String = "android:ThemeOverlay.Material.Dark",
+    maxPercentDifference: Double = DefaultMaxPercentDifference,
+    snapshotHandler: SnapshotHandler = determineHandler(maxPercentDifference),
+    renderExtensions: Set<RenderExtension> = setOf(),
+    renderingMode: SessionParams.RenderingMode = SessionParams.RenderingMode.NORMAL
+): Paparazzi {
+    return Paparazzi(
+        deviceConfig = deviceConfig,
+        theme = theme,
+        maxPercentDifference = maxPercentDifference,
+        snapshotHandler = snapshotHandler,
+        renderExtensions = renderExtensions,
+        renderingMode = renderingMode
+    )
+}
---
+++ roboscreenshots/src/main/java/com/google/android/horologist/paparazzi/WearSnapshotHandler.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.paparazzi
+
+import app.cash.paparazzi.Snapshot
+import app.cash.paparazzi.SnapshotHandler
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import java.awt.geom.Ellipse2D
+import java.awt.image.BufferedImage
+
+@ExperimentalHorologistApi
+public class WearSnapshotHandler(
+    private val delegate: SnapshotHandler,
+    private val round: Boolean = true
+) : SnapshotHandler {
+    override fun close() {
+        delegate.close()
+    }
+
+    private fun frameImage(image: BufferedImage): BufferedImage {
+        val newImage = BufferedImage(image.width, image.height, image.type)
+        newImage.createGraphics().apply {
+            clip = Ellipse2D.Float(0f, 0f, image.height.toFloat(), image.width.toFloat())
+            drawImage(image, 0, 0, image.width, image.height, null)
+            dispose()
+        }
+        return newImage
+    }
+
+    override fun newFrameHandler(
+        snapshot: Snapshot,
+        frameCount: Int,
+        fps: Int
+    ): SnapshotHandler.FrameHandler {
+        val delegateFrameHandler = delegate.newFrameHandler(snapshot, frameCount, fps)
+        return object : SnapshotHandler.FrameHandler {
+            override fun close() {
+                delegateFrameHandler.close()
+            }
+
+            override fun handle(image: BufferedImage) {
+                val processedImage = if (round) {
+                    frameImage(image)
+                } else {
+                    image
+                }
+
+                delegateFrameHandler.handle(processedImage)
+            }
+        }
+    }
+}

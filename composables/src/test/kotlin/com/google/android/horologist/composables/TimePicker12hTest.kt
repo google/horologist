a1a6f045800643d8cf17de42812f64747bb7da09
Yuri Schimke <yuri@schimke.ee>
2023-04-03 12:01:08 +0100

    Migrate composables screenshot tests (#1160)

--- composables/src/test/kotlin/com/google/android/horologist/composables/TimePicker12hTest.kt
+++ composables/src/test/kotlin/com/google/android/horologist/composables/TimePicker12hTest.kt
@@ -19,20 +19,18 @@
 package com.google.android.horologist.composables
 
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
-import com.google.android.horologist.paparazzi.WearPaparazzi
-import org.junit.Ignore
-import org.junit.Rule
+import com.google.android.horologist.screenshots.ScreenshotTest
 import org.junit.Test
 import java.time.LocalTime
 
-@Ignore("Waiting for interactive support for paparazzi")
-class TimePicker12hTest {
-    @get:Rule
-    val paparazzi = WearPaparazzi()
+class TimePicker12hTest: ScreenshotTest() {
+    init {
+        screenTimeText = {}
+    }
 
     @Test
     fun datePickerInitial() {
-        paparazzi.snapshot {
+        takeScreenshot {
             TimePickerWith12HourClock(
                 time = LocalTime.of(10, 10, 0),
                 onTimeConfirm = {}

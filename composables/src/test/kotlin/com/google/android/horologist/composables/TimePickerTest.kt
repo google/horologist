63e0840d70d12c6c22b1216edee781ff5a21015b
Yuri Schimke <yuri@schimke.ee>
2023-04-03 11:47:37 +0100

    composables

--- composables/src/test/kotlin/com/google/android/horologist/composables/TimePickerTest.kt
+++ composables/src/test/kotlin/com/google/android/horologist/composables/TimePickerTest.kt
@@ -19,9 +19,7 @@
 package com.google.android.horologist.composables
 
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
-import com.google.android.horologist.paparazzi.WearPaparazzi
-import org.junit.Ignore
-import org.junit.Rule
+import com.google.android.horologist.screenshots.ScreenshotTest
 import org.junit.Test
 import java.time.LocalTime
 
@@ -32,7 +30,7 @@ class TimePickerTest {
 
     @Test
     fun datePickerInitial() {
-        paparazzi.snapshot {
+        takeScreenshot {
             TimePicker(
                 time = LocalTime.of(10, 10, 0),
                 onTimeConfirm = {}

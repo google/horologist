4de7c82f0380bcd1ab02f50c4a84b9b9b111d71d
Yuri Schimke <yuri@schimke.ee>
2023-04-03 11:16:54 +0100

    Fix

--- auth-ui/src/test/java/com/google/android/horologist/auth/ui/common/screens/prompt/SignInPromptScreenTest.kt
+++ auth-ui/src/test/java/com/google/android/horologist/auth/ui/common/screens/prompt/SignInPromptScreenTest.kt
@@ -31,6 +31,10 @@ import com.google.android.horologist.test.toolbox.composables.positionedState
 import org.junit.Test
 
 class SignInPromptScreenTest: ScreenshotTest() {
+    init {
+        screenTimeText = {}
+    }
+
 
     @Test
     fun idle() {

0adc584b97ea2780d7cb8228996e78a2f03acc72
hardikagr <109944466+hardikagr@users.noreply.github.com>
2023-04-11 16:06:18 +0530

    Adding animation for header of PlayerScreen (#1142)

--- media-ui/src/main/java/com/google/android/horologist/media/ui/components/animated/MarqueeTextMediaDisplay.kt
+++ media-ui/src/main/java/com/google/android/horologist/media/ui/components/animated/MarqueeTextMediaDisplay.kt
@@ -16,13 +16,20 @@
 
 package com.google.android.horologist.media.ui.components.animated
 
+import androidx.annotation.FloatRange
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.with
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.clearAndSetSemantics
-import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.wear.compose.material.MaterialTheme
@@ -38,26 +45,45 @@ import com.google.android.horologist.composables.MarqueeText
 public fun MarqueeTextMediaDisplay(
     modifier: Modifier = Modifier,
     title: String? = null,
-    artist: String? = null
+    artist: String? = null,
+    enterTransitionDelay: Int = 60,
+    subtextTransitionDelay: Int = 30,
+    @FloatRange(from = 0.0, to = 1.0) transitionLength: Float = 0.125f
 ) {
+    fun getTransitionAnimation(delay: Int = 0): ContentTransform {
+        return slideInHorizontally(animationSpec = tween(delayMillis = delay + enterTransitionDelay)) {
+            Math.round(it * transitionLength).toInt()
+        } + fadeIn(animationSpec = tween(delayMillis = delay + enterTransitionDelay)) with
+            slideOutHorizontally(animationSpec = tween(delayMillis = delay)) {
+                Math.round(-it * transitionLength).toInt()
+            } + fadeOut(animationSpec = tween(delayMillis = delay))
+    }
+
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        MarqueeText(
-            text = title.orEmpty(),
-            modifier = Modifier.fillMaxWidth(0.7f).clearAndSetSemantics {
-                contentDescription = title.orEmpty()
-            },
-            color = MaterialTheme.colors.onBackground,
-            style = MaterialTheme.typography.button,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = artist.orEmpty(),
-            modifier = Modifier.fillMaxWidth(0.8f),
-            color = MaterialTheme.colors.onBackground,
-            textAlign = TextAlign.Center,
-            overflow = TextOverflow.Ellipsis,
-            maxLines = 1,
-            style = MaterialTheme.typography.body2
-        )
+        AnimatedContent(targetState = title, transitionSpec = { getTransitionAnimation() }) {
+                currentTitle ->
+            MarqueeText(
+                text = currentTitle.orEmpty(),
+                modifier = Modifier.fillMaxWidth(0.7f),
+                color = MaterialTheme.colors.onBackground,
+                style = MaterialTheme.typography.button,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        AnimatedContent(
+            targetState = artist,
+            transitionSpec = { getTransitionAnimation(subtextTransitionDelay) }
+        ) { currentArtist ->
+            Text(
+                text = currentArtist.orEmpty(),
+                modifier = Modifier.fillMaxWidth(0.8f),
+                color = MaterialTheme.colors.onBackground,
+                textAlign = TextAlign.Center,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                style = MaterialTheme.typography.body2
+            )
+        }
     }
 }

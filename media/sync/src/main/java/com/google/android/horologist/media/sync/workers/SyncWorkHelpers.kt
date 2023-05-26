79476de19a24a36bbdd6b73467f82bb628498292
Yuri Schimke <yuri@schimke.ee>
2023-05-26 07:21:54 +0100

    Update SyncWorkHelpers.kt

--- media/sync/src/main/java/com/google/android/horologist/media/sync/workers/SyncWorkHelpers.kt
+++ media/sync/src/main/java/com/google/android/horologist/media/sync/workers/SyncWorkHelpers.kt
@@ -20,7 +20,6 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
 import androidx.annotation.DrawableRes
 import androidx.core.app.NotificationCompat
 import androidx.work.Constraints

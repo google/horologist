7b95f0504055e0fa23cbdb20906cd9ae585e1d2d
plksharma30 <plksharma30@gmail.com>
2023-08-02 10:38:59 +0530

    changed fields of CustomActionMediaButton to non nullable

--- media/ui/src/main/java/com/google/android/horologist/media/ui/components/CustomActionMediaButton.kt
+++ media/ui/src/main/java/com/google/android/horologist/media/ui/components/CustomActionMediaButton.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * https://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,13 +26,17 @@ import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.ButtonDefaults
 import coil.compose.AsyncImage
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
 
-/** A base button for custom action media controls. */
+/**
+ * A base button for custom action media controls.
+ */
+@ExperimentalHorologistApi
 @Composable
 public fun CustomActionMediaButton(
     onClick: () -> Unit,
-    contentDescription: String?,
-    iconUri: String?,
+    contentDescription: String,
+    iconUri: String,
     colors: Color,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,

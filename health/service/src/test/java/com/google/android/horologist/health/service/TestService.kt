b84f645d4df85fa0e475a2e4c9376a469e5ac221
Yuri Schimke <yuri@schimke.ee>
2023-08-22 18:40:02 +0100

    Add Service connection API (#1596)
    ---------
    
    Co-authored-by: Gustavo Pagani <gustavopagani@google.com>

---
+++ health/service/src/test/java/com/google/android/horologist/health/service/TestService.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.health.service
+
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import androidx.lifecycle.LifecycleService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class TestService : LifecycleService() {
+    var name: ComponentName? = null
+    val localBinder = LocalBinder()
+
+    override fun onBind(intent: Intent): IBinder {
+        return localBinder
+    }
+
+    fun doSomething(): String {
+        return "Something"
+    }
+
+    /** Local clients will use this to access the service. */
+    inner class LocalBinder : Binder() {
+        fun getService() = this@TestService
+
+        val flow: Flow<String> = flowOf("Something 1", "Something 2")
+    }
+}

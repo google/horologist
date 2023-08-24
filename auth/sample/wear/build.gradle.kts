d2145fa1d04be008bf8c7ba1178cd9717c72de0c
Yuri Schimke <yuri@schimke.ee>
2023-08-24 18:18:18 +0100

    Revert

--- auth/sample/wear/build.gradle.kts
+++ auth/sample/wear/build.gradle.kts
@@ -126,7 +126,7 @@ dependencies {
     implementation(libs.playservices.wearable)
 
     debugImplementation(libs.compose.ui.tooling)
-    debugImplementation(libs.androidx.wear.tooling.preview)
+    implementation(libs.androidx.wear.tooling.preview)
     debugImplementation(projects.composeTools)
     releaseCompileOnly(projects.composeTools)
 

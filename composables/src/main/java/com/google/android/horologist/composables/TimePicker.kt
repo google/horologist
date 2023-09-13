94f33184f0a38b01e8588f120fd6b0a8b6292abd
Yuri Schimke <yuri@schimke.ee>
2023-09-13 13:19:47 +0100

    More space for Picker growth

--- composables/src/main/java/com/google/android/horologist/composables/TimePicker.kt
+++ composables/src/main/java/com/google/android/horologist/composables/TimePicker.kt
@@ -215,7 +215,7 @@ public fun TimePicker(
                     val pickerGroupItems = mutableListOf(
                         pickerGroupItemWithRSB(
                             pickerState = hourState,
-                            modifier = Modifier.size(46.dp, 100.dp).offset(x = (-3).dp),
+                            modifier = Modifier.size(46.dp, 100.dp).offset(x = (-3.0).dp),
                             onSelected = {
                                 onPickerSelected(
                                     FocusableElementsTimePicker.HOURS,
@@ -227,7 +227,7 @@ public fun TimePicker(
                         ),
                         pickerGroupItemWithRSB(
                             pickerState = minuteState,
-                            modifier = Modifier.size(46.dp, 100.dp).offset(x = (3).dp),
+                            modifier = Modifier.size(46.dp, 100.dp).offset(x = (2.5).dp),
                             onSelected = {
                                 onPickerSelected(
                                     FocusableElementsTimePicker.MINUTES,

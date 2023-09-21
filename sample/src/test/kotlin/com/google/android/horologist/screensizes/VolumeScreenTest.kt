f187a3f1f53c8bf95270cac303f331da34331a83
Yuri Schimke <yuri@schimke.ee>
2023-09-21 08:24:43 +0100

    More screen size previews

---
+++ sample/src/test/kotlin/com/google/android/horologist/screensizes/VolumeScreenTest.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.screensizes
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.Scaffold
+import com.google.android.horologist.audio.AudioOutput
+import com.google.android.horologist.audio.VolumeState
+import com.google.android.horologist.audio.ui.VolumePositionIndicator
+import com.google.android.horologist.audio.ui.VolumeScreen
+import com.google.android.horologist.audio.ui.components.toAudioOutputUi
+import com.google.android.horologist.audio.ui.mapper.VolumeUiStateMapper
+import com.google.android.horologist.compose.tools.Device
+
+class VolumeScreenTest(device: Device) : ScreenSizeTest(device = device, showTimeText = false) {
+
+    @Composable
+    override fun Content() {
+        VolumeScreenTestCase()
+    }
+}
+
+
+@Composable
+fun VolumeScreenTestCase() {
+    val volumeState = VolumeState(
+        current = 5,
+        max = 10,
+    )
+    val audioOutput = AudioOutput.BluetoothHeadset("id", "Pixelbuds")
+
+    val volumeUiState = VolumeUiStateMapper.map(volumeState = volumeState)
+    Scaffold(
+        positionIndicator = {
+            VolumePositionIndicator(
+                volumeUiState = { volumeUiState },
+            )
+        },
+    ) {
+        VolumeScreen(
+            volume = { volumeUiState },
+            audioOutputUi = audioOutput.toAudioOutputUi(),
+            increaseVolume = { },
+            decreaseVolume = { },
+            onAudioOutputClick = { },
+            showVolumeIndicator = false,
+        )
+    }
+}

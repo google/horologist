4d8f225967d336201099f4b6f6eab36d68d1e11b
Yuri Schimke <yuri@schimke.ee>
2023-10-05 17:16:53 +0100

    Backport the rotary changes

--- compose-layout/src/main/java/com/google/android/horologist/compose/rotaryinput/RotaryInputAccumulator.kt
+++ compose-layout/src/main/java/com/google/android/horologist/compose/rotaryinput/RotaryInputAccumulator.kt
@@ -69,7 +69,7 @@ internal class RotaryInputAccumulator(
             1f
         } else if (isLowRes && scrollPixels < 0f) { // For negative tick in low res devices
             -1f
-        } else if (isLowRes /** && scrollPixels == 0f **/) { // For no tick in low res devices
+        } else if (isLowRes) { // && scrollPixels == 0f // For no tick in low res devices
             0f
         } else { // Take it as is for high res devices
             scrollPixels

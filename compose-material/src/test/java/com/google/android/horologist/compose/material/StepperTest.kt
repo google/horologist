d1d3721deeba171658544c1213ea690d64a521bc
Yuri Schimke <yuri@schimke.ee>
2023-10-09 14:10:58 +0100

    Fix

--- compose-material/src/test/java/com/google/android/horologist/compose/material/StepperTest.kt
+++ compose-material/src/test/java/com/google/android/horologist/compose/material/StepperTest.kt
@@ -29,7 +29,7 @@ import org.junit.Test
 class StepperTest : ScreenshotBaseTest(
     params = ScreenshotTestRule.screenshotTestRuleParams {
         screenTimeText = {}
-    }
+    },
 ) {
 
     @Test

ace5a0406a92e7e496db6e75097e887b4820fcc7
Yuri Schimke <yuri@schimke.ee>
2023-11-13 12:11:37 +1000

    Test repro

---
+++ sample/src/main/java/com/google/android/horologist/scratch/ScratchPreview.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(ExperimentalWearFoundationApi::class)
+
+package com.google.android.horologist.scratch
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.wear.compose.foundation.ExperimentalWearFoundationApi
+import androidx.wear.compose.foundation.expandableItem
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
+import androidx.wear.compose.foundation.rememberExpandableState
+import androidx.wear.compose.material.ListHeader
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.ui.tooling.preview.WearPreviewLargeRound
+import com.google.android.horologist.compose.rotaryinput.rotaryWithScroll
+
+@WearPreviewLargeRound
+@Composable
+fun WearPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        val state = rememberScalingLazyListState()
+        val expandableState = rememberExpandableState()
+        ScalingLazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .rotaryWithScroll(state),
+            state = state
+        ) {
+            item {
+                ListHeader {
+                    Text(text = "Main")
+                }
+            }
+            expandableItem(expandableState) {
+                Text(text = "I am $it")
+            }
+            items(10) {
+                Text(text = "Item $it")
+            }
+        }
+    }
+
+}
---
+++ sample/src/test/kotlin/com/google/android/horologist/scratch/ScratchTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.scratch
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
+import androidx.compose.ui.test.printToString
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(RobolectricTestRunner::class)
+@Config(
+    sdk = [33],
+    qualifiers = "w227dp-h227dp-small-notlong-round-watch-xhdpi-keyshidden-nonav",
+)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+class ScratchTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun onDateConfirm_called_when_confirm_clicked() {
+        composeTestRule.setContent {
+            WearPreview()
+        }
+
+        println(composeTestRule.onRoot().printToString())
+
+        composeTestRule.onNodeWithText("I am true").assertExists()
+        composeTestRule.onNodeWithText("I am false").assertDoesNotExist()
+    }
+}

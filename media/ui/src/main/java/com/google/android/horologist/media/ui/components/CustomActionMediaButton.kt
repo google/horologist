fa52318415c1987c2a03ccc960d8786c13a9a3fa
laiyichin <laiyichin@gmail.com>
2023-11-15 22:21:47 +0000

    Add default icon size to custom action media button aligning to the media button (redline specs) (#1815)

--- media/ui/src/main/java/com/google/android/horologist/media/ui/components/CustomActionMediaButton.kt
+++ media/ui/src/main/java/com/google/android/horologist/media/ui/components/CustomActionMediaButton.kt
@@ -16,10 +16,13 @@
 
 package com.google.android.horologist.media.ui.components
 
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Button
@@ -40,7 +43,9 @@ public fun CustomActionMediaButton(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
     colors: ButtonColors = ButtonDefaults.iconButtonColors(),
+    iconSize: Dp = 30.dp,
     tapTargetSize: DpSize = DpSize(48.dp, 60.dp),
+    iconAlign: Alignment.Horizontal = Alignment.CenterHorizontally,
 ) {
     Button(
         onClick = onClick,
@@ -52,6 +57,22 @@ public fun CustomActionMediaButton(
             model = iconUri,
             contentDescription = contentDescription,
             colorFilter = ColorFilter.tint(colors.contentColor(enabled = enabled).value),
+            modifier = Modifier
+                .size(iconSize)
+                .run {
+                    when (iconAlign) {
+                        Alignment.Start -> {
+                            offset(x = -7.5.dp)
+                        }
+                        Alignment.End -> {
+                            offset(x = 7.5.dp)
+                        }
+                        else -> {
+                            this
+                        }
+                    }
+                }
+                .align(Alignment.Center),
         )
     }
 }

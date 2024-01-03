3d98add137c387658b605dc6fdae3581403af0c5
Katrina Paikena <104018303+kpaikena@users.noreply.github.com>
2024-01-03 18:04:19 +0000

    Media button ripple fix (#1842)
    Make the button click ripple in previous/next buttons bigger and round rather than oval.

--- media/ui/build.gradle.kts
+++ media/ui/build.gradle.kts
@@ -124,6 +124,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodelktx)
     implementation(libs.compose.material.iconscore)
     implementation(libs.compose.material.iconsext)
+    implementation(libs.compose.material.ripple)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     implementation(libs.lottie.compose)

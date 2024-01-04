ab383e17460534107b068fa6afcd6107b73bc8d8
Yuri Schimke <yuri@schimke.ee>
2024-01-04 09:39:20 +0000

    Testing Vertical Pager

--- sample/src/main/java/com/google/android/horologist/scratch/ScratchActivity.kt
+++ sample/src/main/java/com/google/android/horologist/scratch/ScratchActivity.kt
@@ -14,40 +14,59 @@
  * limitations under the License.
  */
 
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.google.android.horologist.scratch
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.border
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pages
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.Saver
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.wear.compose.foundation.lazy.AutoCenteringParams
-import androidx.wear.compose.foundation.lazy.ScalingLazyListAnchorType
-import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.HorizontalPageIndicator
 import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Scaffold
-import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TimeText
-import androidx.wear.compose.material.curvedText
-import androidx.wear.compose.material.scrollAway
+import androidx.wear.compose.material.PageIndicatorDefaults
+import androidx.wear.compose.material.PageIndicatorState
+import androidx.wear.compose.material.PageIndicatorStyle
+import androidx.wear.compose.material.PositionIndicatorDefaults
+import androidx.wear.compose.material.PositionIndicatorState
+import androidx.wear.compose.material.PositionIndicatorVisibility
+import androidx.wear.compose.navigation.SwipeDismissableNavHost
+import androidx.wear.compose.navigation.composable
+import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
+import com.google.android.horologist.compose.layout.AppScaffold
 import com.google.android.horologist.compose.layout.ScalingLazyColumn
-import com.google.android.horologist.compose.layout.ScalingLazyColumnDefaults
+import com.google.android.horologist.compose.layout.ScreenScaffold
 import com.google.android.horologist.compose.layout.rememberColumnState
+import com.google.android.horologist.compose.material.Button
+import com.google.android.horologist.compose.material.ToggleChip
+import com.google.android.horologist.compose.material.ToggleChipToggleControl
+import com.google.android.horologist.compose.pager.ClippedBox
+import com.google.android.horologist.compose.pager.HorizontalPagerDefaults
+import com.google.android.horologist.compose.pager.PageScreenIndicatorState
 
 class ScratchActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,164 +78,179 @@ class ScratchActivity : ComponentActivity() {
     }
 }
 
-data class Offsets(
-    val index: Int = 0,
-    val offset: Int = 0,
-)
-
 @Composable
 fun WearApp() {
-    var settings by rememberSaveable(stateSaver = Settings.Saver) {
-        mutableStateOf(Settings())
-    }
-
-    key(settings) {
-        val initialOffset = settings.initialOffset
-        val itemHeight = settings.itemHeight
-        val autoCentering = settings.autoCentering
-        val anchorType = settings.anchorType
-
-        val columnState = rememberColumnState(
-            ScalingLazyColumnDefaults.scalingLazyColumnDefaults(
-                initialCenterIndex = initialOffset.index,
-                initialCenterOffset = initialOffset.offset,
-                autoCentering = autoCentering,
-                anchorType = anchorType,
-            ),
-        )
-
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            timeText = {
-                TimeText(
-                    modifier = Modifier
-                        .scrollAway(
-                            columnState.state,
-                            columnState.initialScrollPosition.index,
-                            columnState.initialScrollPosition.offsetPx.dp,
-                        ),
-                    startCurvedContent = {
-                        curvedText("${columnState.state.centerItemIndex}/${columnState.state.centerItemScrollOffset}")
-                    },
-                )
-            },
+    var discreteRsb: Boolean by remember { mutableStateOf(false) }
+    var usePositionIndicator: Boolean by remember { mutableStateOf(false) }
+    var screenSize: Boolean by remember { mutableStateOf(true) }
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    val navController = rememberSwipeDismissableNavController()
+    AppScaffold {
+        SwipeDismissableNavHost(
+            navController = navController,
+            startDestination = "menu"
         ) {
-            ScalingLazyColumn(
-                columnState = columnState,
-            ) {
-                item {
-                    val text = "Initial Offset: ${initialOffset.index} / ${initialOffset.offset}"
-                    FixedHeightChip(text, itemHeight, onClick = {
-                        settings = settings.copy(
-                            initialOffsetsMode = (settings.initialOffsetsMode + 1) % Settings.initialOffsets.size,
-                        )
-                    })
-                }
-                item {
-                    val text =
-                        "Auto Centering: ${Settings.autoCenterings[settings.autoCenteringMode].first}"
-                    FixedHeightChip(text, itemHeight, onClick = {
-                        settings = settings.copy(
-                            autoCenteringMode = (settings.autoCenteringMode + 1) % Settings.autoCenterings.size,
-                        )
-                    })
-                }
-                item {
-                    val text = "Anchor Type: ${Settings.anchorTypes[settings.anchorTypeMode].first}"
-                    FixedHeightChip(text, itemHeight, onClick = {
-                        settings = settings.copy(
-                            anchorTypeMode = (settings.anchorTypeMode + 1) % Settings.anchorTypes.size,
-                        )
-                    })
-                }
-                item {
-                    val text = "Item Height: ${settings.itemHeight}"
-                    FixedHeightChip(text, itemHeight, onClick = {
-                        settings = settings.copy(
-                            itemHeightMode = (settings.itemHeightMode + 1) % Settings.itemHeights.size,
-                        )
-                    })
+            composable("menu") {
+                ScreenScaffold {
+                    ScalingLazyColumn(columnState = rememberColumnState()) {
+                        item {
+                            Button(
+                                onClick = {
+                                    navController.navigate(
+                                        "pager"
+                                    )
+                                },
+                                imageVector = Icons.Default.Pages,
+                                contentDescription = "Open Pager"
+                            )
+                        }
+                        item {
+                            ToggleChip(
+                                checked = discreteRsb,
+                                onCheckedChanged = { discreteRsb = !discreteRsb },
+                                label = "Discrete RSB",
+                                toggleControl = ToggleChipToggleControl.Checkbox
+                            )
+                        }
+                        item {
+                            ToggleChip(
+                                checked = usePositionIndicator,
+                                onCheckedChanged = { usePositionIndicator = !usePositionIndicator },
+                                label = "Position Indicator",
+                                toggleControl = ToggleChipToggleControl.Checkbox
+                            )
+                        }
+                        item {
+                            ToggleChip(
+                                checked = screenSize,
+                                onCheckedChanged = { screenSize = !screenSize },
+                                label = "Single Screen Content",
+                                toggleControl = ToggleChipToggleControl.Checkbox
+                            )
+                        }
+                    }
                 }
             }
-            Canvas(modifier = Modifier.fillMaxSize()) {
-                drawLine(
-                    Color.Red,
-                    Offset(0f, size.height / 2f),
-                    Offset(size.width, size.height / 2f),
-                )
+
+            composable("pager") {
+                val pagerState = rememberPagerState {
+                    4
+                }
+                ScreenScaffold {
+                    VerticalPagerScreen(
+                        state = pagerState,
+                        discreteRsb = discreteRsb,
+                        usePositionIndicator = usePositionIndicator
+                    ) { page ->
+                        Box(
+                            modifier = Modifier
+                                .background(if (page % 2 == 0) Color.DarkGray else Color.Black)
+                                .fillMaxWidth()
+                                .height(screenHeight * if (screenSize) 1 else 2),
+                            contentAlignment = Alignment.Center
+                        ) {
+
+                        }
+                    }
+                }
             }
         }
     }
 }
 
-data class Settings(
-    val initialOffsetsMode: Int = 0,
-    val autoCenteringMode: Int = 1,
-    val itemHeightMode: Int = 0,
-    val anchorTypeMode: Int = 0,
+
+@Composable
+fun VerticalPageIndicator(
+    pageIndicatorState: PageIndicatorState,
+    modifier: Modifier = Modifier,
+    indicatorStyle: PageIndicatorStyle = PageIndicatorDefaults.style(),
+    selectedColor: Color = MaterialTheme.colors.onBackground,
+    unselectedColor: Color = selectedColor.copy(alpha = 0.3f),
+    indicatorSize: Dp = 6.dp,
+    spacing: Dp = 4.dp,
+    indicatorShape: Shape = CircleShape
 ) {
-    val initialOffset: Offsets = initialOffsets[initialOffsetsMode]
-    val itemHeight: Int = itemHeights[itemHeightMode]
-    val autoCentering: AutoCenteringParams? = autoCenterings[autoCenteringMode].second
-    val anchorType: ScalingLazyListAnchorType = anchorTypes[anchorTypeMode].second
+    HorizontalPageIndicator(
+        pageIndicatorState = pageIndicatorState,
+        modifier = modifier,
+        indicatorStyle = indicatorStyle,
+        selectedColor = selectedColor,
+        unselectedColor = unselectedColor,
+        indicatorSize = indicatorSize,
+        spacing = spacing,
+        indicatorShape = indicatorShape
+    )
+}
 
-    companion object {
-        val initialOffsets = listOf(
-            Offsets(0, 0),
-            Offsets(1, 0),
-            Offsets(1, -20),
-            Offsets(1, 20),
-            Offsets(2, 0),
-        )
+class PagerStatePositionIndicatorState(val state: PagerState) : PositionIndicatorState {
+    override val positionFraction: Float
+        get() = if (state.pageCount <= 1) {
+            1f
+        } else {
+            (state.currentPage + state.currentPageOffsetFraction) / (state.pageCount - 1)
+        }
 
-        val autoCenterings = listOf(
-            Pair("null", null),
-            Pair("0/0", AutoCenteringParams(0, 0)),
-            Pair("1/0", AutoCenteringParams(1, 0)),
-            Pair("2/0", AutoCenteringParams(2, 0)),
-            Pair("3/0", AutoCenteringParams(3, 0)),
-        )
+    override fun sizeFraction(scrollableContainerSizePx: Float): Float =
+        1f / state.pageCount
 
-        val itemHeights = listOf(40, 80, 120)
-
-        val anchorTypes = listOf(
-            Pair("Center", ScalingLazyListAnchorType.ItemCenter),
-            Pair("Start", ScalingLazyListAnchorType.ItemStart),
-        )
-
-        val Saver = Saver<Settings, List<Int>>(
-            save = {
-                listOf(
-                    it.initialOffsetsMode,
-                    it.autoCenteringMode,
-                    it.itemHeightMode,
-                    it.anchorTypeMode,
-                )
-            },
-            restore = {
-                Settings(it[0], it[1], it[2], it[3])
-            },
-        )
-    }
+    override fun visibility(scrollableContainerSizePx: Float): PositionIndicatorVisibility =
+        PositionIndicatorVisibility.AutoHide
 }
 
 @Composable
-fun FixedHeightChip(text: String, itemHeight: Int, onClick: () -> Unit) {
-    Box(
-        modifier = Modifier
-            .height(itemHeight.dp)
-            .fillMaxWidth()
-            .border(1.dp, Color.DarkGray),
+public fun PositionIndicator(
+    pagerState: PagerState,
+    modifier: Modifier = Modifier,
+    reverseDirection: Boolean = false,
+    fadeInAnimationSpec: AnimationSpec<Float> = PositionIndicatorDefaults.visibilityAnimationSpec,
+    fadeOutAnimationSpec: AnimationSpec<Float> = PositionIndicatorDefaults.visibilityAnimationSpec,
+    positionAnimationSpec: AnimationSpec<Float> =
+        PositionIndicatorDefaults.positionAnimationSpec
+) = androidx.wear.compose.material.PositionIndicator(
+    state = PagerStatePositionIndicatorState(
+        state = pagerState
+    ),
+    indicatorHeight = 50.dp,
+    indicatorWidth = 4.dp,
+    paddingHorizontal = 5.dp,
+    modifier = modifier,
+    reverseDirection = reverseDirection,
+    fadeInAnimationSpec = fadeInAnimationSpec,
+    fadeOutAnimationSpec = fadeOutAnimationSpec,
+    positionAnimationSpec = positionAnimationSpec
+)
+
+@Composable
+public fun VerticalPagerScreen(
+    state: PagerState,
+    discreteRsb: Boolean,
+    usePositionIndicator: Boolean,
+    modifier: Modifier = Modifier,
+    clip: Boolean = true,
+    content: @Composable ((Int) -> Unit),
+) {
+    ScreenScaffold(
+        modifier = modifier.fillMaxSize(),
+        positionIndicator = {
+            if (usePositionIndicator) {
+                PositionIndicator(pagerState = state)
+            } else {
+                VerticalPageIndicator(pageIndicatorState = PageScreenIndicatorState(state))
+            }
+        }
     ) {
-        Chip(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = onClick,
-            label = {
-                Text(
-                    text = text,
-                    style = MaterialTheme.typography.caption3,
-                )
-            },
-        )
+        VerticalPager(
+            modifier = Modifier.fillMaxSize(),
+            state = state,
+            flingBehavior = HorizontalPagerDefaults.flingParams(state),
+        ) { page ->
+            if (clip) {
+                ClippedBox(state) {
+                    content(page)
+                }
+            } else {
+                content(page)
+            }
+        }
     }
 }

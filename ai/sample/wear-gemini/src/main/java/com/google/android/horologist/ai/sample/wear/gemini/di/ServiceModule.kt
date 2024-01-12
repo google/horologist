f1119c173e884e634cb0fc6616ac2aba89fe78b5
Yuri Schimke <yuri@schimke.ee>
2024-01-12 12:41:31 +0000

    Cleanup

--- ai/sample/wear-gemini/src/main/java/com/google/android/horologist/ai/sample/wear/gemini/di/ServiceModule.kt
+++ ai/sample/wear-gemini/src/main/java/com/google/android/horologist/ai/sample/wear/gemini/di/ServiceModule.kt
@@ -32,7 +32,7 @@ import javax.inject.Qualifier
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
     // From https://github.com/google/generative-ai-android/blob/main/generativeai-android-sample/app/src/main/kotlin/com/google/ai/sample/GenerativeAiViewModelFactory.kt
-    
+
     @ServiceScoped
     @Provides
     fun modelConfig(): GenerationConfig = generationConfig {

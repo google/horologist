f91af9c941f61ad78893e9da58d7c1c2dcea50f9
Yuri Schimke <yuri@schimke.ee>
2024-01-12 10:40:44 +0000

    Cleanup

--- ai/ui/src/debug/src/main/kotlin/com/google/android/horologist/ai/ui/components/ResponsePreview.kt
+++ ai/ui/src/debug/src/main/kotlin/com/google/android/horologist/ai/ui/components/ResponsePreview.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-
+package com.google.android.horologist.ai.ui.components
 
 @WearPreviewLargeRound
 @Composable
--- ai/ui/src/debug/src/main/kotlin/com/google/android/horologist/ai/ui/components/TextPromptDisplayPreview.kt
+++ ai/ui/src/debug/src/main/kotlin/com/google/android/horologist/ai/ui/components/TextPromptDisplayPreview.kt
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+package com.google.android.horologist.ai.ui.components
 
 @WearPreviewLargeRound
 @Composable

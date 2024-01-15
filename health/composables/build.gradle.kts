ab0326e98a7233c3ed89ea6f2c5f492132185f01
Yuri Schimke <yuri@schimke.ee>
2024-01-15 09:11:49 +0000

    Reduce libraries to minSdk 25

--- health/composables/build.gradle.kts
+++ health/composables/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 25
+        minSdk = 30
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

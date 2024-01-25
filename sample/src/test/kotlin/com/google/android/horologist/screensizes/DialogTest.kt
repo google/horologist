566df8bf091bdf4bdce982157f9c595531f199ce
Yuri Schimke <yschimke@google.com>
2024-01-25 08:52:10 +0000

    Demonstrate a responsive dialog layout

--- sample/src/test/kotlin/com/google/android/horologist/screensizes/DialogTest.kt
+++ sample/src/test/kotlin/com/google/android/horologist/screensizes/DialogTest.kt
@@ -17,15 +17,28 @@
 package com.google.android.horologist.screensizes
 
 import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumnDefaults
+import androidx.wear.compose.foundation.lazy.ScalingLazyListAnchorType
+import androidx.wear.compose.material.ChipDefaults
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.dialog.Alert
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
 import com.google.android.horologist.compose.layout.ScalingLazyColumnState
 import com.google.android.horologist.compose.layout.rememberColumnState
 import com.google.android.horologist.compose.material.AlertContent
@@ -34,12 +47,15 @@ import com.google.android.horologist.compose.material.ResponsiveDialogContent
 import com.google.android.horologist.compose.material.ToggleChip
 import com.google.android.horologist.compose.material.ToggleChipToggleControl
 import com.google.android.horologist.compose.tools.Device
+import com.google.android.horologist.screenshots.ScreenshotTestRule
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import kotlin.math.sqrt
 
 class DialogTest(device: Device) : ScreenSizeTest(
     device = device,
     showTimeText = false,
+    recordMode = ScreenshotTestRule.RecordMode.Record
 ) {
 
     @Composable
@@ -50,6 +66,7 @@ class DialogTest(device: Device) : ScreenSizeTest(
             onCancelButtonClick = {},
             onOKButtonClick = {},
             message = "Tap the button below to install it on your phone.",
+            columnState = rememberColumnState(responsiveDialog())
         )
     }
 
@@ -94,7 +111,7 @@ class DialogTest(device: Device) : ScreenSizeTest(
 
             screenshotTestRule.takeScreenshot()
         }) {
-            columnState = rememberColumnState()
+            columnState = rememberColumnState(responsiveDialog())
 
             ResponsiveDialogContent(
                 title = {
@@ -125,6 +142,98 @@ class DialogTest(device: Device) : ScreenSizeTest(
                         onCheckedChanged = {},
                         label = "Don't show again",
                         toggleControl = ToggleChipToggleControl.Checkbox,
+                    )
+                }
+            }
+        }
+    }
+
+    @ExperimentalHorologistApi
+    public fun responsiveDialog(
+        verticalArrangement: Arrangement.Vertical =
+            Arrangement.spacedBy(
+                space = 4.dp,
+                alignment = Alignment.Top,
+            ),
+        horizontalPaddingPercent: Float = 0.0416f,
+        rotaryMode: ScalingLazyColumnState.RotaryMode? = ScalingLazyColumnState.RotaryMode.Scroll,
+        hapticsEnabled: Boolean = true,
+        userScrollEnabled: Boolean = true,
+    ): ScalingLazyColumnState.Factory {
+        fun calculateVerticalOffsetForChip(
+            viewportDiameter: Float,
+            horizontalPaddingPercent: Float,
+        ): Dp {
+            val childViewHeight: Float = ChipDefaults.Height.value
+            val childViewWidth: Float = viewportDiameter * (1.0f - (2f * horizontalPaddingPercent))
+            val radius = viewportDiameter / 2f
+            return (
+                radius -
+                    sqrt(
+                        (radius - childViewHeight + childViewWidth * 0.5f) * (radius - childViewWidth * 0.5f),
+                    ) -
+                    childViewHeight * 0.5f
+                ).dp
+        }
+
+        return object : ScalingLazyColumnState.Factory {
+            @Composable
+            override fun create(): ScalingLazyColumnState {
+                val density = LocalDensity.current
+                val configuration = LocalConfiguration.current
+                val screenWidthDp = configuration.screenWidthDp.toFloat()
+                val screenHeightDp = configuration.screenHeightDp.toFloat()
+
+                return remember {
+                    val padding = screenWidthDp * horizontalPaddingPercent
+                    val topPaddingDp: Dp = (screenWidthDp * 0.1456f).dp
+                    val bottomPaddingDp: Dp = if (configuration.isScreenRound) {
+                        calculateVerticalOffsetForChip(screenWidthDp, horizontalPaddingPercent)
+                    } else {
+                        0.dp
+                    }
+                    val contentPadding = PaddingValues(
+                        start = padding.dp,
+                        end = padding.dp,
+                        top = topPaddingDp,
+                        bottom = bottomPaddingDp,
+                    )
+
+                    val sizeRatio = ((screenWidthDp - 192) / (233 - 192).toFloat()).coerceIn(0f, 1.5f)
+                    val presetRatio = 0f
+
+                    val minElementHeight = lerp(0.2f, 0.157f, sizeRatio)
+                    val maxElementHeight = lerp(0.6f, 0.472f, sizeRatio).coerceAtLeast(minElementHeight)
+                    val minTransitionArea = lerp(0.35f, lerp(0.35f, 0.393f, presetRatio), sizeRatio)
+                    val maxTransitionArea = lerp(0.55f, lerp(0.55f, 0.593f, presetRatio), sizeRatio)
+
+                    val scalingParams = ScalingLazyColumnDefaults.scalingParams(
+                        minElementHeight = minElementHeight,
+                        maxElementHeight = maxElementHeight,
+                        minTransitionArea = minTransitionArea,
+                        maxTransitionArea = maxTransitionArea,
+                    )
+
+                    val screenHeightPx =
+                        with(density) { screenHeightDp.dp.roundToPx() }
+                    val topPaddingPx = with(density) { topPaddingDp.roundToPx() }
+                    val topScreenOffsetPx = screenHeightPx / 2 - topPaddingPx
+
+                    val initialScrollPosition = ScalingLazyColumnState.ScrollPosition(
+                        index = 0,
+                        offsetPx = topScreenOffsetPx,
+                    )
+                    ScalingLazyColumnState(
+                        initialScrollPosition = initialScrollPosition,
+                        autoCentering = null,
+                        anchorType = ScalingLazyListAnchorType.ItemStart,
+                        rotaryMode = rotaryMode,
+                        verticalArrangement = verticalArrangement,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        contentPadding = contentPadding,
+                        scalingParams = scalingParams,
+                        hapticsEnabled = hapticsEnabled,
+                        userScrollEnabled = userScrollEnabled,
                     )
                 }
             }

dbbc0ae7ab6e3c79ba686e5828702804c4c9442f
Yuri Schimke <yuri@schimke.ee>
2024-03-13 13:06:55 +0000

    Publish the tile_tracking_enabled item (#2118)

--- datalayer/sample/wear/src/main/java/com/google/android/horologist/datalayer/sample/TileSync.kt
+++ datalayer/sample/wear/src/main/java/com/google/android/horologist/datalayer/sample/TileSync.kt
@@ -19,16 +19,23 @@ package com.google.android.horologist.datalayer.sample
 import android.content.Context
 import androidx.concurrent.futures.await
 import androidx.wear.tiles.TileService
+import com.google.android.gms.wearable.PutDataRequest
+import com.google.android.horologist.data.WearDataLayerRegistry
 import com.google.android.horologist.datalayer.watch.WearDataLayerAppHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.tasks.await
 
 class TileSync(
+    val registry: WearDataLayerRegistry,
     val wearAppHelper: WearDataLayerAppHelper,
 ) {
     val executor = Dispatchers.Default.asExecutor()
 
     suspend fun trackInstalledTiles(context: Context) {
+        registry.dataClient
+            .putDataItem(PutDataRequest.create("/tile_tracking_enabled")).await()
+
         val myTilesList = listOf(
             SampleTileService::class.java.name,
         )

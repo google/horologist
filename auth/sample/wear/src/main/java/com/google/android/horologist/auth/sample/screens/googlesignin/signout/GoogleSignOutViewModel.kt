9d2612990fb821e377f1f8ed8cab87860917a43e
Yuri Schimke <yschimke@google.com>
2024-04-17 11:40:07 +0100

    Avoid deprecations

--- auth/sample/wear/src/main/java/com/google/android/horologist/auth/sample/screens/googlesignin/signout/GoogleSignOutViewModel.kt
+++ auth/sample/wear/src/main/java/com/google/android/horologist/auth/sample/screens/googlesignin/signout/GoogleSignOutViewModel.kt
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+@file:Suppress("DEPRECATION")
+
 package com.google.android.horologist.auth.sample.screens.googlesignin.signout
 
 import android.util.Log

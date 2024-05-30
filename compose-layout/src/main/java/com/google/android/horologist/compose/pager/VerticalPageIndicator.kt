7dc78e5bf6692d6814699fc40e48ebff90f9c1ed
Yuri Schimke <yschimke@google.com>
2024-05-30 17:40:04 +0100

    Fixes

--- compose-layout/src/main/java/com/google/android/horologist/compose/pager/VerticalPageIndicator.kt
+++ compose-layout/src/main/java/com/google/android/horologist/compose/pager/VerticalPageIndicator.kt
@@ -18,7 +18,6 @@ package com.google.android.horologist.compose.pager
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier

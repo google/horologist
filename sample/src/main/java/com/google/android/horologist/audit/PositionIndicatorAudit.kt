31c070a34120163b33b00f5da60ccfbf8e0a5a45
Yuri Schimke <yuri@schimke.ee>
2024-07-04 17:57:13 +0100

    More

--- sample/src/main/java/com/google/android/horologist/audit/PositionIndicatorAudit.kt
+++ sample/src/main/java/com/google/android/horologist/audit/PositionIndicatorAudit.kt
@@ -16,9 +16,75 @@
 
 package com.google.android.horologist.audit
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.MaterialTheme
+import com.google.android.horologist.compose.layout.ScalingLazyColumn
+import com.google.android.horologist.compose.layout.ScalingLazyColumnDefaults
+import com.google.android.horologist.compose.layout.ScalingLazyColumnDefaults.padding
+import com.google.android.horologist.compose.layout.ScreenScaffold
+import com.google.android.horologist.compose.layout.rememberResponsiveColumnState
 
 @Composable
 fun PositionIndicatorAudit(route: AuditNavigation.PositionIndicator.Audit) {
-    
+    val (initial, itemCount) = when (route.config) {
+        AuditNavigation.PositionIndicator.Config.TopLong -> {
+            Pair(0, 100)
+        }
+
+        AuditNavigation.PositionIndicator.Config.TopShort -> {
+            Pair(0, 10)
+        }
+
+        AuditNavigation.PositionIndicator.Config.BottomLong -> {
+            Pair(100, 100)
+        }
+
+        AuditNavigation.PositionIndicator.Config.BottomShort -> {
+            Pair(100, 10)
+        }
+
+        AuditNavigation.PositionIndicator.Config.MiddleShort -> {
+            Pair(5, 10)
+        }
+
+        AuditNavigation.PositionIndicator.Config.MiddleLong -> {
+            Pair(50, 100)
+        }
+    }
+
+    val columnState = rememberResponsiveColumnState(
+        contentPadding = padding(
+            first = ScalingLazyColumnDefaults.ItemType.Chip,
+            last = ScalingLazyColumnDefaults.ItemType.Chip
+        )
+    )
+
+    ScreenScaffold(scrollState = columnState) {
+        ScalingLazyColumn(columnState) {
+            items(itemCount) {
+                Box(
+                    modifier = Modifier
+                        .height(20.dp)
+                        .fillMaxWidth()
+                        .clip(MaterialTheme.shapes.small)
+                        .background(
+                            Color.DarkGray.copy(alpha = 0.5f)
+                        )
+                )
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        columnState.state.scrollToItem(initial)
+    }
 }

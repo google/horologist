02ac7294a65b6f0188b1204e397f1bbdd1f57199
Yuri Schimke <yschimke@google.com>
2024-07-04 16:01:37 +0100

    Some screenshot tests

--- sample/src/main/java/com/google/android/horologist/audit/PageIndicatorAudit.kt
+++ sample/src/main/java/com/google/android/horologist/audit/PageIndicatorAudit.kt
@@ -16,9 +16,39 @@
 
 package com.google.android.horologist.audit
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.wear.compose.material.Text
+import com.google.android.horologist.compose.pager.PagerScreen
 
 @Composable
 fun PageIndicatorAudit(route: AuditNavigation.PageIndicator.Audit) {
-    
+    val pagerState = when (route.config) {
+        AuditNavigation.PageIndicator.Config.TwoDots -> {
+            rememberPagerState { 2 }
+        }
+
+        AuditNavigation.PageIndicator.Config.FourDots -> {
+            rememberPagerState { 4 }
+        }
+
+        AuditNavigation.PageIndicator.Config.Left5Plus -> {
+            rememberPagerState(initialPage = 0) { 6 }
+        }
+
+        AuditNavigation.PageIndicator.Config.Right5Plus -> {
+            rememberPagerState(initialPage = 5) { 6 }
+        }
+    }
+
+    PagerScreen(pagerState) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text("This page intentionally left blank.\npage $it", textAlign = TextAlign.Center)
+        }
+    }
 }

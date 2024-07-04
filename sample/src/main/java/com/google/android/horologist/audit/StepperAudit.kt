e7aed5544d14cfd5292f7212d9363aca2a59722e
Yuri Schimke <yschimke@google.com>
2024-07-04 15:41:17 +0100

    Some screenshot tests

--- sample/src/main/java/com/google/android/horologist/audit/StepperAudit.kt
+++ sample/src/main/java/com/google/android/horologist/audit/StepperAudit.kt
@@ -16,9 +16,63 @@
 
 package com.google.android.horologist.audit
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import com.google.android.horologist.audio.ui.R
+import com.google.android.horologist.compose.layout.ScreenScaffold
+import com.google.android.horologist.compose.material.Stepper
 
 @Composable
 fun StepperAudit(route: AuditNavigation.Stepper.Audit) {
-    
+    ScreenScaffold(timeText = {}) {
+        when (route.config) {
+            AuditNavigation.Stepper.Config.ButtonAndIcon -> {
+                Stepper(
+                    value = 0.5f,
+                    onValueChange = {},
+                    steps = 10,
+                    increaseIcon = { Icon(Icons.Default.Add, contentDescription = "") },
+                    decreaseIcon = {
+                        Icon(Icons.Default.Remove, contentDescription = "")
+                    }) { }
+            }
+
+            AuditNavigation.Stepper.Config.ButtonOnly -> {
+                Text("TBC")
+            }
+
+            AuditNavigation.Stepper.Config.TextOnly -> {
+                Stepper(
+                    value = 0.5f,
+                    onValueChange = {},
+                    steps = 10,
+                    increaseIcon = { Text("Plus") },
+                    decreaseIcon = {
+                        Text("Minus")
+                    }) { }
+            }
+
+            AuditNavigation.Stepper.Config.VolumeIndicator -> {
+                Stepper(value = 0.5f, onValueChange = {}, steps = 10) {
+                    Text(
+                        stringResource(id = R.string.horologist_volume_screen_volume_label),
+                        style = MaterialTheme.typography.button.copy(
+                            color = MaterialTheme.colors.onBackground,
+                            fontWeight = FontWeight.Normal,
+                        ),
+                        maxLines = 1,
+                        overflow = TextOverflow.Clip,
+                    )
+                }
+            }
+        }
+    }
 }

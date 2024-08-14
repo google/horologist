324914ab840837ac8254f5547a6487f2f054933e
Yuri Schimke <yschimke@google.com>
2024-08-14 08:55:51 +0100

    Fix publishing and deps

--- media/audio-ui/src/main/java/com/google/android/horologist/audio/ui/components/animated/AnimatedSetVolumeButton.kt
+++ media/audio-ui/src/main/java/com/google/android/horologist/audio/ui/components/animated/AnimatedSetVolumeButton.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -53,7 +54,7 @@ public fun AnimatedSetVolumeButton(
     )
     val lottieAnimatable = rememberLottieAnimatable()
 
-    var lastVolume by remember { mutableStateOf(volumeUiState.current) }
+    var lastVolume by remember { mutableIntStateOf(volumeUiState.current) }
 
     LaunchedEffect(volumeUiState) {
         val lastVolumeBefore = lastVolume

c05c2971bdcad4c2b4ec237566b2bd51cb3ab470
Jason Le <anhtuan.le@sigma.se>
2025-07-02 10:16:32 +0000

    Add Check Your Phone screen
    Add auth-composables-material3 submodule and also Check Your Phone screen

---
+++ auth/composables-material3/src/main/java/com/google/android/horologist/auth/composables/material3/theme/Color.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.auth.composables.material3.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val horologist_primary: Color = Color(0xFFD3E3FD)
+internal val horologist_on_primary: Color = Color(0xFF001944)
+internal val horologist_primary_container: Color = Color(0xFF04409F)
+internal val horologist_on_primary_container: Color = Color(0xFFD3E3FD)
+internal val horologist_on_background: Color = Color(0xFFFFFFFF)

9a1582cfd3bdcf429c540fae7dbf377394c8c8f3
Yuri Schimke <yuri@schimke.ee>
2025-07-11 19:24:20 +0100

    Refactor hiltViewModel import (#2652)
    The `androidx.hilt.navigation.compose.hiltViewModel` import has been changed to `androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel` across multiple sample app files. This change reflects an update in the Hilt library where the `hiltViewModel` function is now located in a different package.

--- datalayer/sample/phone/src/main/java/com/google/android/horologist/datalayer/sample/screens/inappprompts/custom/reengage/ReEngageCustomPromptDemoScreen.kt
+++ datalayer/sample/phone/src/main/java/com/google/android/horologist/datalayer/sample/screens/inappprompts/custom/reengage/ReEngageCustomPromptDemoScreen.kt
@@ -48,7 +48,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.android.horologist.datalayer.sample.R
 

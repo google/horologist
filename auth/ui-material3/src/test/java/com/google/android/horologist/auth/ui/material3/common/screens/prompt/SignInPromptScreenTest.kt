0d8990e3a1fb3a985cc5c46cc3ad8fec90d0e208
Caleb Allen <caleballen@google.com>
2025-11-17 17:21:59 +0000

    Import correct AccountUiModel in tests

--- auth/ui-material3/src/test/java/com/google/android/horologist/auth/ui/material3/common/screens/prompt/SignInPromptScreenTest.kt
+++ auth/ui-material3/src/test/java/com/google/android/horologist/auth/ui/material3/common/screens/prompt/SignInPromptScreenTest.kt
@@ -25,6 +25,7 @@ import androidx.wear.compose.foundation.lazy.ScalingLazyListScope
 import androidx.wear.compose.material.Text
 import com.google.android.horologist.auth.composables.material3.buttons.GuestModeButton
 import com.google.android.horologist.auth.composables.material3.buttons.SignInButton
+import com.google.android.horologist.auth.composables.material3.models.AccountUiModel
 import com.google.android.horologist.screenshots.rng.WearLegacyScreenTest
 import org.junit.Test
 

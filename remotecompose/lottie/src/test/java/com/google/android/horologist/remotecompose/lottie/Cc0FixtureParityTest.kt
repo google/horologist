fc7901343c193b5dc7e344545fd41de10487f0eb
Mark Yavorskyi <myavorskyi@google.com>
2026-09-14 11:16:46 +0000

    Add core Lottie feature regression tests (Part 2/3)
    Adds regression tests covering core Lottie features:
    - Bundled fixture regression cases
    - CC0 sample fixtures and parity tests
    - Dynamic color playback
    - Multi-dimensional position interpolation
    - Stroke miter limits and beveling
    - TrimScopeRegressionTest (trim path scoping)
    - RepeaterScopeTest & RepeaterOpacityTest (repeaters & opacity ramps)
    - MergePathsRegressionTest & MergeScopeTest (boolean path operations)
    - RoundedCornersRegressionTest & RoundedPathParityTest (corner rounding)
    - CompoundFillRegressionTest (even-odd fills & hole cutting)
    - LayerStretchRegressionTest (precomp time stretch)
    - PlaybackEffectsRegressionTest (time remapping & masks)
    - RemainingCompositingRegressionTest (alpha & luminance mattes)
    
    Failing tests on main AST/renderer are annotated with @Ignore("TODO: Fix failure on main AST/renderer").
    
    TAG=agy
    CONV=6430017c-8336-4849-a261-1ab75c80d930

---
+++ remotecompose/lottie/src/test/java/com/google/android/horologist/remotecompose/lottie/Cc0FixtureParityTest.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.horologist.remotecompose.lottie
+
+import org.junit.Ignore
+import org.junit.Test
+
+/** Real CC0 exports compared numerically with the pinned lottie-android renderer. */
+class Cc0FixtureParityTest : FixtureParityHarness() {
+  @Ignore("TODO: Fix failure on main AST/renderer")
+  @Test
+  fun colorEase() = compare(R.raw.cc0_color_ease)
+
+  @Ignore("TODO: Fix failure on main AST/renderer")
+  @Test
+  fun gradientAlpha() = compare(R.raw.cc0_gradient_alpha)
+
+  @Ignore("TODO: Fix failure on main AST/renderer")
+  @Test
+  fun multidimensional() = compare(R.raw.cc0_multidimensional)
+
+  @Ignore("TODO: Fix failure on main AST/renderer")
+  @Test
+  fun positionHold() = compare(R.raw.cc0_position_hold)
+
+  @Ignore("TODO: Fix failure on main AST/renderer")
+  @Test
+  fun positionPath() = compare(R.raw.cc0_position_path)
+
+  @Ignore("TODO: Fix failure on main AST/renderer")
+  @Test
+  fun precompStretch() = compare(R.raw.cc0_precomp_stretch, maxForegroundError = 0.01)
+}
